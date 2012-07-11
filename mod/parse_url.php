48f67ead9bbebe868e0ab585d56538540ba80339
Michael Vogel <icarus@dabo.de>
2012-07-12 01:17:33 +0200

    parse_url: complete new code for fetching website information

--- mod/parse_url.php
+++ mod/parse_url.php
@@ -1,7 +1,163 @@
 <?php
+if(!function_exists('deletenode')) {
+	function deletenode(&$doc, $node)
+	{
+		$xpath = new DomXPath($doc);
+		$list = $xpath->query("//".$node);
+		foreach ($list as $child)
+			$child->parentNode->removeChild($child);
+	}
+}
 
-require_once('library/HTML5/Parser.php');
-require_once('library/HTMLPurifier.auto.php');
+function parseurl_getsiteinfo($url) {
+	$siteinfo = array();
+
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_URL, $url);
+	curl_setopt($ch, CURLOPT_HEADER, 1);
+	curl_setopt($ch, CURLOPT_NOBODY, 0);
+	curl_setopt($ch, CURLOPT_TIMEOUT, 3);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($ch,CURLOPT_USERAGENT,'Opera/9.64(Windows NT 5.1; U; de) Presto/2.1.1');
+
+	$header = curl_exec($ch);
+	curl_close($ch);
+
+	if (preg_match('/charset=(.*?)\n/', $header, $matches))
+		$charset = trim(array_pop($matches));
+	else
+		$charset = "utf-8";
+
+	$pos = strpos($header, "\r\n\r\n");
+
+	if ($pos)
+		$body = trim(substr($header, $pos));
+	else
+		$body = $header;
+
+	$body = mb_convert_encoding($body, "UTF-8", $charset);
+	$body = mb_convert_encoding($body, 'HTML-ENTITIES', "UTF-8");
+
+	$doc = new DOMDocument();
+	@$doc->loadHTML($body);
+
+	deletenode($doc, 'style');
+	deletenode($doc, 'script');
+	deletenode($doc, 'option');
+	deletenode($doc, 'h1');
+	deletenode($doc, 'h2');
+	deletenode($doc, 'h3');
+	deletenode($doc, 'h4');
+	deletenode($doc, 'h5');
+	deletenode($doc, 'h6');
+	deletenode($doc, 'ol');
+	deletenode($doc, 'ul');
+
+	$xpath = new DomXPath($doc);
+
+	$list = $xpath->query("head/title");
+	foreach ($list as $node)
+		$siteinfo["title"] =  html_entity_decode($node->nodeValue, ENT_QUOTES, "UTF-8");
+
+	$list = $xpath->query("head/meta[@name]");
+	foreach ($list as $node) {
+		$attr = array();
+		if ($node->attributes->length)
+                        foreach ($node->attributes as $attribute)
+                                $attr[$attribute->name] = $attribute->value;
+
+		$attr["content"] = html_entity_decode($attr["content"], ENT_QUOTES, "UTF-8");
+
+		switch (strtolower($attr["name"])) {
+			case "fulltitle":
+				$siteinfo["title"] = $attr["content"];
+				break;
+			case "description":
+				$siteinfo["text"] = $attr["content"];
+				break;
+			case "dc.title":
+				$siteinfo["title"] = $attr["content"];
+				break;
+			case "dc.description":
+				$siteinfo["text"] = $attr["content"];
+				break;
+		}
+	}
+
+	$list = $xpath->query("head/meta[@property]");
+	foreach ($list as $node) {
+		$attr = array();
+		if ($node->attributes->length)
+                        foreach ($node->attributes as $attribute)
+                                $attr[$attribute->name] = $attribute->value;
+
+		$attr["content"] = html_entity_decode($attr["content"], ENT_QUOTES, "UTF-8");
+
+		switch (strtolower($attr["property"])) {
+			case "og:image":
+				$siteinfo["image"] = $attr["content"];
+				break;
+			case "og:title":
+				$siteinfo["title"] = $attr["content"];
+				break;
+			case "og:description":
+				$siteinfo["text"] = $attr["content"];
+				break;
+		}
+	}
+
+	if ($siteinfo["image"] == "") {
+                require_once('include/Photo.php');
+                $list = $xpath->query("//img[@src]");
+                foreach ($list as $node) {
+                        $attr = array();
+                        if ($node->attributes->length)
+                                foreach ($node->attributes as $attribute)
+                                        $attr[$attribute->name] = $attribute->value;
+
+                        // guess mimetype from headers or filename
+                        $type = guess_image_type($attr["src"],true);
+
+                        $i = fetch_url($attr["src"]);
+                        $ph = new Photo($i, $type);
+
+                        if(($ph->getWidth() > 200) and ($ph->getHeight() > 200))
+                                $siteinfo["image"] = $attr["src"];
+                }
+        }
+
+	if ($siteinfo["text"] == "") {
+		$text = "";
+
+		$list = $xpath->query("//div[@class='article']");
+		foreach ($list as $node)
+			$text .= " ".trim($node->nodeValue);
+
+		if ($text == "") {
+			$list = $xpath->query("//div[@class='content']");
+			foreach ($list as $node)
+				$text .= " ".trim($node->nodeValue);
+		}
+
+		// If none text was found then take the paragraph content
+		if ($text == "") {
+			$list = $xpath->query("//p");
+			foreach ($list as $node)
+				$text .= " ".trim($node->nodeValue);
+		}
+
+		if ($text != "") {
+			$text = trim(str_replace(array("\n", "\r"), array(" ", " "), $text));
+
+			while (strpos($text, "  "))
+				$text = trim(str_replace("  ", " ", $text));
+
+			$siteinfo["text"] = html_entity_decode(substr($text,0,350), ENT_QUOTES, "UTF-8").'...';
+		}
+	}
+
+	return($siteinfo);
+}
 
 function arr_add_hashes(&$item,$k) {
 	$item = '#' . $item;
@@ -40,12 +196,10 @@ function parse_url_content(&$a) {
 
 	logger('parse_url: ' . $url);
 
-
 	if($textmode)
 		$template = $br . '[bookmark=%s]%s[/bookmark]%s' . $br;
 	else
 		$template = "<br /><a class=\"bookmark\" href=\"%s\" >%s</a>%s<br />";
-
 
 	$arr = array('url' => $url, 'text' => '');
 
@@ -60,187 +214,53 @@ function parse_url_content(&$a) {
 	if($url && $title && $text) {
 
 		if($textmode)
-			$text = $br . $br . '[quote]' . $text . '[/quote]' . $br;
+			$text = $br . $br . '[quote]' . trim($text) . '[/quote]' . $br;
 		else
-			$text = '<br /><br /><blockquote>' . $text . '</blockquote><br />';
+			$text = '<br /><br /><blockquote>' . trim($text) . '</blockquote><br />';
 
 		$title = str_replace(array("\r","\n"),array('',''),$title);
 
 		$result = sprintf($template,$url,($title) ? $title : $url,$text) . $str_tags;
 
-		logger('parse_url (unparsed): returns: ' . $result); 
+		logger('parse_url (unparsed): returns: ' . $result);
 
 		echo $result;
 		killme();
 	}
 
+	$siteinfo = parseurl_getsiteinfo($url);
 
-	if($url) {
-		$s = fetch_url($url);
+	if($siteinfo["title"] == "") {
+		echo sprintf($template,$url,$url,'') . $str_tags;
+		killme();
 	} else {
-		echo '';
-		killme();
+		$image = $siteinfo["image"];
+		$text = $siteinfo["text"];
+		$title = $siteinfo["title"];
 	}
 
-//	logger('parse_url: data: ' . $s, LOGGER_DATA);
+	if ($image != "") {
+		$i = fetch_url($image);
+		if($i) {
+			require_once('include/Photo.php');
+			// guess mimetype from headers or filename
+			$type = guess_image_type($image,true);
 
-	if(! $s) {
-		echo sprintf($template,$url,$url,'') . $str_tags;
-		killme();
-	}
-
-	$matches = '';
-	$c = preg_match('/\<head(.*?)\>(.*?)\<\/head\>/ism',$s,$matches);
-	if($c) {
-//		logger('parse_url: header: ' . $matches[2], LOGGER_DATA);
-		try {
-			$domhead = HTML5_Parser::parse($matches[2]);
-		} catch (DOMException $e) {
-			logger('scrape_dfrn: parse error: ' . $e);
-		}
-		if($domhead)
-			logger('parsed header');
-	}
-
-	if(! $title) {
-		if(strpos($s,'<title>')) {
-			$title = substr($s,strpos($s,'<title>')+7,64);
-			if(strpos($title,'<') !== false)
-				$title = strip_tags(substr($title,0,strpos($title,'<')));
-		}
-	}
-
-	$config = HTMLPurifier_Config::createDefault();
-	$config->set('Cache.DefinitionImpl', null);
-	$purifier = new HTMLPurifier($config);
-	$s = $purifier->purify($s);
-
-//	logger('purify_output: ' . $s);
-
-	try {
-		$dom = HTML5_Parser::parse($s);
-	} catch (DOMException $e) {
-		logger('scrape_dfrn: parse error: ' . $e);
-	}
-
-	if(! $dom) {
-		echo sprintf($template,$url,$url,'') . $str_tags;
-		killme();
-	}
-
-	$items = $dom->getElementsByTagName('title');
-
-	if($items) {
-		foreach($items as $item) {
-			$title = trim($item->textContent);
-			break;
-		}
-	}
-
-
-	if(! $text) {
-		$divs = $dom->getElementsByTagName('div');
-		if($divs) {
-			foreach($divs as $div) {
-				$class = $div->getAttribute('class');
-				if($class && (stristr($class,'article') || stristr($class,'content'))) {
-					$items = $div->getElementsByTagName('p');
-					if($items) {
-						foreach($items as $item) {
-							$text = $item->textContent;
-							if(stristr($text,'<script')) {
-								$text = '';
-								continue;
-							}
-							$text = strip_tags($text);
-							if(strlen($text) < 100) {
-								$text = '';
-								continue;
-							}
-							$text = substr($text,0,250) . '...' ;
-							break;
-						}
-					}
-				}
-				if($text)
-					break;
-			}
-		}
-
-		if(! $text) {
-			$items = $dom->getElementsByTagName('p');
-			if($items) {
-				foreach($items as $item) {
-					$text = $item->textContent;
-					if(stristr($text,'<script'))
-						continue;
-					$text = strip_tags($text);
-					if(strlen($text) < 100) {
-						$text = '';
-						continue;
-					}
-					$text = substr($text,0,250) . '...' ;
-					break;
-				}
-			}
-		}
-	}
-
-	if(! $text) {
-		logger('parsing meta');
-		$items = (isset($domhead) && is_object($domhead) ? $domhead->getElementsByTagName('meta') : null);
-		if($items) {
-			foreach($items as $item) {
-				$property = $item->getAttribute('property');
-				if($property && (stristr($property,':description'))) {
-
-					$text = $item->getAttribute('content');
-					if(stristr($text,'<script')) {
-						$text = '';
-						continue;
-					}
-					$text = strip_tags($text);
-
-
-					$text = substr($text,0,250) . '...' ;
-				}
-				if($property && (stristr($property,':image'))) {
-
-					$image = $item->getAttribute('content');
-					if(stristr($text,'<script')) {
-						$image = '';
-						continue;
-					}
-					$image = strip_tags($image);
-					
-					$i = fetch_url($image);
-					if($i) {
-						require_once('include/Photo.php');
-						// guess mimetype from headers or filename
-						$type = guess_image_type($image,true);
-
-						$ph = new Photo($i, $type);
-						if($ph->is_valid()) {
-							if($ph->getWidth() > 300 || $ph->getHeight() > 300) {
-								$ph->scaleImage(300);
-								$new_width = $ph->getWidth();
-								$new_height = $ph->getHeight();
-								if($textmode)
-									$image = $br . $br . '[img=' . $new_width . 'x' . $new_height . ']' . $image . '[/img]';
-								else
-									$image = '<br /><br /><img height="' . $new_height . '" width="' . $new_width . '" src="' .$image . '" alt="photo" />';
-							}
-							else {
-								if($textmode) 
-									$image = $br . $br . '[img]' . $image . '[/img]';
-								else
-									$image = '<br /><br /><img src="' . $image . '" alt="photo" />';
-							}
-						}
-						else
-							$image = '';
-					
-					}
+			$ph = new Photo($i, $type);
+			if($ph->is_valid()) {
+				if($ph->getWidth() > 300 || $ph->getHeight() > 300) {
+					$ph->scaleImage(300);
+					$new_width = $ph->getWidth();
+					$new_height = $ph->getHeight();
+					if($textmode)
+						$image = $br . $br . '[img=' . $new_width . 'x' . $new_height . ']' . $image . '[/img]';
+					else
+						$image = '<br /><br /><img height="' . $new_height . '" width="' . $new_width . '" src="' .$image . '" alt="photo" />';
+				} else {
+					if($textmode)
+						$image = $br.$br.'[img]'.$image.'[/img]';
+					else
+						$image = '<br /><br /><img src="'.$image.'" alt="photo" />';
 				}
 			}
 		}
@@ -248,19 +268,19 @@ function parse_url_content(&$a) {
 
 	if(strlen($text)) {
 		if($textmode)
-			$text = $br .$br . '[quote]' . $text . '[/quote]' . $br ;
+			$text = $br.$br.'[quote]'.trim($text).'[/quote]'.$br ;
 		else
-			$text = '<br /><br /><blockquote>' . $text . '</blockquote><br />';
+			$text = '<br /><br /><blockquote>'.trim($text).'</blockquote><br />';
 	}
 
 	if($image) {
-		$text = $image . $br . $text;
+		$text = $image.$br.$text;
 	}
 	$title = str_replace(array("\r","\n"),array('',''),$title);
 
 	$result = sprintf($template,$url,($title) ? $title : $url,$text) . $str_tags;
 
-	logger('parse_url: returns: ' . $result); 
+	logger('parse_url: returns: ' . $result);
 
 	echo $result;
 	killme();
