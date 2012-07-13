88711deceb1f3c1187c6aa649adcfdeed45a9016
Zach Prezkuta <fermion@gmx.com>
2012-07-13 09:09:01 -0600

    allow live updates for unlikes and undislikes

--- mod/profile.php
+++ mod/profile.php
@@ -204,6 +204,8 @@ function profile_content(&$a, $update = 0) {
 
 		$r = q("SELECT distinct(parent) AS `item_id`, `contact`.`uid` AS `contact-uid`
 			FROM `item` LEFT JOIN `contact` ON `contact`.`id` = `item`.`contact-id`
+			WHERE `item`.`uid` = %d AND `item`.`visible` = 1 AND
+			(`item`.`deleted` = 0 OR item.verb = '" . ACTIVITY_LIKE ."' OR item.verb = '" . ACTIVITY_DISLIKE . "')
 			WHERE `item`.`uid` = %d AND `item`.`visible` = 1 AND `item`.`deleted` = 0
 			and `item`.`moderated` = 0 and `item`.`unseen` = 1
 			AND `contact`.`blocked` = 0 AND `contact`.`pending` = 0
