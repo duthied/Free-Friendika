c052c6afca08eff24c0b4b95c7bde1e9ee2c4e31
Domovoy <domovoy@errlock.org>
2012-07-24 01:21:02 +0200

    Database can now be automatically updated

--- boot.php
+++ boot.php
@@ -12,7 +12,7 @@ require_once('include/cache.php');
 define ( 'FRIENDICA_PLATFORM',     'Friendica');
 define ( 'FRIENDICA_VERSION',      '3.0.1412' );
 define ( 'DFRN_PROTOCOL_VERSION',  '2.23'    );
-define ( 'DB_UPDATE_VERSION',      1153      );
+define ( 'DB_UPDATE_VERSION',      1154      );
 
 define ( 'EOL',                    "<br />\r\n"     );
 define ( 'ATOM_TIME',              'Y-m-d\TH:i:s\Z' );
