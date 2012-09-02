05dda2f226b263e82249bd43d3511051b9c6a49a
pixelroot <info@pixelbits.de>
2012-09-02 13:11:17 +0200

    new file:   view/theme/smoothly/back-gradient.jpg 	new file:   view/theme/smoothly/calendar.png 	new file:   view/theme/smoothly/comment_item.tpl 	new file:   view/theme/smoothly/connect.png 	new file:   view/theme/smoothly/contact_template.tpl 	new file:   view/theme/smoothly/conversation.tpl 	new file:   view/theme/smoothly/group_drop.tpl 	new file:   view/theme/smoothly/group_edit.tpl 	new file:   view/theme/smoothly/group_side.tpl 	new file:   view/theme/smoothly/groups.png 	new file:   view/theme/smoothly/icons.png 	new file:   view/theme/smoothly/icons.svg 	new file:   view/theme/smoothly/jot-header.tpl 	new file:   view/theme/smoothly/jot.tpl 	new file:   view/theme/smoothly/lock.cur 	new file:   view/theme/smoothly/login-bg.gif 	new file:   view/theme/smoothly/lrarrow.gif 	new file:   view/theme/smoothly/mail_head.tpl 	new file:   view/theme/smoothly/match.tpl 	new file:   view/theme/smoothly/menu-user-pin.jpg 	new file:   view/theme/smoothly/menu-user-pin.png 	new file:   view/theme/smoothly/nav-bg.png 	new file:   view/theme/smoothly/nav.tpl 	new file:   view/theme/smoothly/nets.tpl 	new file:   view/theme/smoothly/next.png 	new file:   view/theme/smoothly/notifications.png 	new file:   view/theme/smoothly/photo-menu.jpg 	new file:   view/theme/smoothly/photo_album.tpl 	new file:   view/theme/smoothly/photo_top.tpl 	new file:   view/theme/smoothly/photo_view.tpl 	new file:   view/theme/smoothly/photography.png 	new file:   view/theme/smoothly/premium.png 	new file:   view/theme/smoothly/prev.png 	new file:   view/theme/smoothly/profile_entry.tpl 	new file:   view/theme/smoothly/profile_vcard.tpl 	new file:   view/theme/smoothly/saved_searches_aside.tpl 	new file:   view/theme/smoothly/screenshot.jpg 	new file:   view/theme/smoothly/screenshot.png 	new file:   view/theme/smoothly/search.png 	new file:   view/theme/smoothly/search_item.tpl 	new file:   view/theme/smoothly/star.png 	new file:   view/theme/smoothly/style.css 	new file:   view/theme/smoothly/theme.php 	new file:   view/theme/smoothly/user.png 	new file:   view/theme/smoothly/wall_item.tpl 	new file:   view/theme/smoothly/wallwall_item.tpl

---
+++ view/theme/smoothly/theme.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+ * Name: Smoothly
+ * Version: Version 0.2
+ * Author: Alex <alex@friendica.pixelbits.de>
+ * Maintainer: Alex alex@friendica.pixelbits.de>
+ * Description: Based on "Test Bubble", optimized for iPad.
+ * Screenshot: <a href="screenshot.png">Screenshot</a>
+ */
+
+$a->page['htmlhead'] .= <<< EOT
+<script>
+$(document).ready(function() {
+
+$('html').click(function() { $("#nav-notifications-menu" ).hide(); });
+});
+</script>
+EOT;
