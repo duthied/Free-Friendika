4990f64927e2ddd65e1a1f4b1ef85fec12fb98a9
Zach Prezkuta <fermion@gmx.com>
2012-10-29 12:03:48 -0600

    make sure that the contact is local for auto redir

--- include/redir.php
+++ include/redir.php
@@ -7,10 +7,23 @@ function auto_redir(&$a, $contact_nick) {
 
 	if(local_user()) {
 
-		$r = q("SELECT id FROM contact WHERE uid = ( SELECT uid FROM user WHERE nickname = '%s' LIMIT 1 ) AND nick = '%s' AND network = '%s' and self = 0 LIMIT 1",
+		// We need to find out if $contact_nick is a user on this hub, and if so, if I
+		// am a contact of that user. However, that user may have other contacts with the
+		// same nickname as me on other hubs or other networks. Exclude these by requiring
+		// that the contact have a local URL. I will be the only person with my nickname at
+		// this URL, so if a result is found, then I am a contact of the $contact_nick user.
+
+		$baseurl = $a->get_baseurl();
+		$domain_st = strpos($baseurl, "://");
+		if($domain_st === false)
+			return;
+		$baseurl = substr($baseurl, $domain_st + 3);
+
+		$r = q("SELECT id FROM contact WHERE uid = ( SELECT uid FROM user WHERE nickname = '%s' LIMIT 1 )
+		        AND nick = '%s' AND self = 0 AND url LIKE '%%%s%%' LIMIT 1",
 			   dbesc($contact_nick),
 			   dbesc($a->user['nickname']),
-			   dbesc(NETWORK_DFRN)
+		       dbesc($baseurl)
 		);
 
 		if((!$r) || (! count($r)) || $r[0]['id'] == remote_user())
@@ -52,7 +65,7 @@ function auto_redir(&$a, $contact_nick) {
 
 		$url = curPageURL();
 
-		logger('check_redir: ' . $r[0]['name'] . ' ' . $sec, LOGGER_DEBUG); 
+		logger('auto_redir: ' . $r[0]['name'] . ' ' . $sec, LOGGER_DEBUG); 
 		$dest = (($url) ? '&destination_url=' . $url : '');
 		goaway ($r[0]['poll'] . '?dfrn_id=' . $dfrn_id 
 			. '&dfrn_version=' . DFRN_PROTOCOL_VERSION . '&type=profile&sec=' . $sec . $dest );
