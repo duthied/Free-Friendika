c6a3b87359b56fe8899eae5432af7b8bd0db9d37
Zach Prezkuta <fermion@gmx.com>
2012-11-01 18:36:11 -0600

    update_display for display live updates

---
+++ mod/update_display.php
@@ -0,0 +1,38 @@
+<?php
+
+// See update_profile.php for documentation
+
+require_once('mod/display.php');
+require_once('include/group.php');
+
+function update_display_content(&$a) {
+
+	$profile_uid = intval($_GET['p']);
+
+	header("Content-type: text/html");
+	echo "<!DOCTYPE html><html><body>\r\n";
+	echo (($_GET['msie'] == 1) ? '<div>' : '<section>');
+
+
+	$text = display_content($a,$profile_uid);
+	$pattern = "/<img([^>]*) src=\"([^\"]*)\"/";
+	$replace = "<img\${1} dst=\"\${2}\"";
+	$text = preg_replace($pattern, $replace, $text);
+
+	$replace = '<br />' . t('[Embedded content - reload page to view]') . '<br />';
+	$pattern = "/<\s*audio[^>]*>(.*?)<\s*\/\s*audio>/i";
+	$text = preg_replace($pattern, $replace, $text);
+	$pattern = "/<\s*video[^>]*>(.*?)<\s*\/\s*video>/i";
+	$text = preg_replace($pattern, $replace, $text);
+	$pattern = "/<\s*embed[^>]*>(.*?)<\s*\/\s*embed>/i";
+	$text = preg_replace($pattern, $replace, $text);
+	$pattern = "/<\s*iframe[^>]*>(.*?)<\s*\/\s*iframe>/i";
+	$text = preg_replace($pattern, $replace, $text);
+
+
+	echo str_replace("\t",'       ',$text);
+	echo (($_GET['msie'] == 1) ? '</div>' : '</section>');
+	echo "</body></html>\r\n";
+	killme();
+
+}
