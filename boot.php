215a7f139caded94553fc84bafca847c7634b955
friendica <info@friendica.com>
2012-12-30 00:29:07 -0800

    make smarty error reporting a bit less chatty

--- boot.php
+++ boot.php
@@ -12,7 +12,7 @@ require_once('library/Mobile_Detect/Mobile_Detect.php');
 require_once('include/features.php');
 
 define ( 'FRIENDICA_PLATFORM',     'Friendica');
-define ( 'FRIENDICA_VERSION',      '3.1.1571' );
+define ( 'FRIENDICA_VERSION',      '3.1.1572' );
 define ( 'DFRN_PROTOCOL_VERSION',  '2.23'    );
 define ( 'DB_UPDATE_VERSION',      1157      );
 
