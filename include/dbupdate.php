2a8e5032e02613df0cc056c17f82ad53be638339
Zach Prezkuta <fermion@gmx.com>
2013-01-14 16:58:32 -0700

    run check_config() in a separate process

---
+++ include/dbupdate.php
@@ -0,0 +1,29 @@
+<?php
+
+require_once("boot.php");
+
+function dbupdate_run(&$argv, &$argc) {
+	global $a, $db;
+
+	if(is_null($a)){
+		$a = new App;
+	}
+  
+	if(is_null($db)) {
+		@include(".htconfig.php");
+		require_once("include/dba.php");
+		$db = new dba($db_host, $db_user, $db_pass, $db_data);
+		        unset($db_host, $db_user, $db_pass, $db_data);
+	}
+
+	load_config('config');
+	load_config('system');
+
+	check_config($a);
+}
+
+if (array_search(__file__,get_included_files())===0){
+  dbupdate_run($argv,$argc);
+  killme();
+}
+
