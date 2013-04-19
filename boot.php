a13e7b8a38f6eb61237c79090f144a60ea9fafe1
friendica <info@friendica.com>
2013-04-19 04:51:43 -0700

    rev update

--- boot.php
+++ boot.php
@@ -12,7 +12,7 @@ require_once('library/Mobile_Detect/Mobile_Detect.php');
 require_once('include/features.php');
 
 define ( 'FRIENDICA_PLATFORM',     'Friendica');
-define ( 'FRIENDICA_VERSION',      '3.1.1660' );
+define ( 'FRIENDICA_VERSION',      '3.1.1675' );
 define ( 'DFRN_PROTOCOL_VERSION',  '2.23'    );
 define ( 'DB_UPDATE_VERSION',      1163      );
 define ( 'EOL',                    "<br />\r\n"     );
