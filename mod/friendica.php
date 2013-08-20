3ff982be9928d6a3fc98d5287d9ddc5687eef5e7
Tobias Diekershoff <tobias.diekershoff@gmx.net>
2013-08-20 11:35:26 +0200

    we use the bugtracker on github now

--- mod/friendica.php
+++ mod/friendica.php
@@ -57,7 +57,7 @@ function friendica_content(&$a) {
 
 	$o .= t('Please visit <a href="http://friendica.com">Friendica.com</a> to learn more about the Friendica project.') . '</p><p>';	
 
-	$o .= t('Bug reports and issues: please visit') . ' ' . '<a href="http://bugs.friendica.com">Bugs.Friendica.com</a></p><p>';
+	$o .= t('Bug reports and issues: please visit') . ' ' . '<a href="https://github.com/friendica/friendica/issues?state=open">the bucktracker at github</a></p><p>';
 	$o .= t('Suggestions, praise, donations, etc. - please email "Info" at Friendica - dot com') . '</p>';
 
 	$o .= '<p></p>';
