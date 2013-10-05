2d1b261fad5b92568b1572b36b3618d149323fa0
Michael Vogel <icarus@dabo.de>
2013-10-05 12:30:10 +0200

    Speed improvements of queries

--- mod/community.php
+++ mod/community.php
@@ -70,7 +70,7 @@ function community_content(&$a, $update = 0) {
 		`contact`.`network`, `contact`.`thumb`, `contact`.`self`, `contact`.`writable`, 
 		`contact`.`id` AS `cid`, `contact`.`uid` AS `contact-uid`,
 		`user`.`nickname`, `user`.`hidewall`
-		FROM `item` FORCE INDEX (`received`) LEFT JOIN `contact` ON `contact`.`id` = `item`.`contact-id`
+		FROM `item` FORCE INDEX (`received`, `wall`) LEFT JOIN `contact` ON `contact`.`id` = `item`.`contact-id`
 		LEFT JOIN `user` ON `user`.`uid` = `item`.`uid`
 		WHERE `item`.`visible` = 1 AND `item`.`deleted` = 0 and `item`.`moderated` = 0
 		AND `item`.`allow_cid` = ''  AND `item`.`allow_gid` = ''
