52b260982c0eec7c18ed4f3b503819fee28e172e
Michael Vogel <icarus@dabo.de>
2014-01-13 07:49:10 +0100

    Avoiding a warning in the error.log

--- include/acl_selectors.php
+++ include/acl_selectors.php
@@ -338,7 +338,7 @@ function populate_acl($user = null,$celeb = false) {
 		'$denycid' => json_encode($perms['deny_cid']),
 		'$denygid' => json_encode($perms['deny_gid']),
 		'$features' => array(
-			"aclautomention"=>(feature_enabled($user,"aclautomention")?"true":"false")
+			"aclautomention"=>(feature_enabled($uid,"aclautomention")?"true":"false")
 		),
 	));
 
