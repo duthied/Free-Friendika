5e3b8b34327aea58dd473e6b268746690d6583db
Michael Vogel <icarus@dabo.de>
2014-09-06 12:38:06 +0200

    Creating a shadow entry for every public post and show it on the community page.

--- include/threads.php
+++ include/threads.php
@@ -13,9 +13,47 @@ function add_thread($itemid) {
 			.implode("`, `", array_keys($item))
 			."`) VALUES ('"
 			.implode("', '", array_values($item))
-			."')" );
+			."')");
 
 	logger("add_thread: Add thread for item ".$itemid." - ".print_r($result, true), LOGGER_DEBUG);
+
+	// Adding a shadow item entry
+	if (($itemid == 0) OR ($item['uid'] == 0))
+		return;
+
+	// Check, if hide-friends is activated - then don't do a shadow entry
+	$r = q("SELECT `hide-friends` FROM `profile` WHERE `is-default` AND `uid` = %d AND NOT `hide-friends`",
+		$item['uid']);
+	if (!count($r))
+		return;
+
+	// Only add a shadow, if the profile isn't hidden
+	$r = q("SELECT `uid` FROM `user` where `uid` = %d AND NOT `hidewall`", $item['uid']);
+	if (!count($r))
+		return;
+
+	$item = q("SELECT * FROM `item` WHERE `id` = %d",
+		intval($itemid));
+
+	if (count($item) AND ($item[0]["visible"] == 1) AND ($item[0]["deleted"] == 0) AND
+		(($item[0]["id"] == $item[0]["parent"]) OR ($item[0]["parent"] == 0)) AND
+		($item[0]["moderated"] == 0) AND ($item[0]["allow_cid"] == '')  AND ($item[0]["allow_gid"] == '') AND
+		($item[0]["deny_cid"] == '') AND ($item[0]["deny_gid"] == '') AND ($item[0]["private"] == 0)) {
+
+		$r = q("SELECT `id` FROM `item` WHERE `uri` = '%s' AND `uid` = 0 LIMIT 1",
+			dbesc($item['uri']));
+
+		if (!$r) {
+			// Preparing public shadow (removing user specific data)
+			require_once("include/items.php");
+			unset($item[0]['id']);
+			$item[0]['uid'] = 0;
+			$item[0]['contact-id'] = 0;
+
+			$public_shadow = item_store($item[0],false);
+			logger("add_thread: Stored public shadow for post ".$itemid." under id ".$public_shadow, LOGGER_DEBUG);
+		}
+	}
 }
 
 function update_thread_uri($itemuri, $uid) {
@@ -27,7 +65,7 @@ function update_thread_uri($itemuri, $uid) {
 }
 
 function update_thread($itemid, $setmention = false) {
-	$items = q("SELECT `uid`, `created`, `edited`, `commented`, `received`, `changed`, `wall`, `private`, `pubmail`, `moderated`, `visible`, `spam`, `starred`, `bookmark`, `contact-id`,
+	$items = q("SELECT `uid`, `uri`, `created`, `edited`, `commented`, `received`, `changed`, `wall`, `private`, `pubmail`, `moderated`, `visible`, `spam`, `starred`, `bookmark`, `contact-id`,
 			`deleted`, `origin`, `forum_mode`, `network`  FROM `item` WHERE `id` = %d AND (`parent` = %d OR `parent` = 0) LIMIT 1", intval($itemid), intval($itemid));
 
 	if (!$items)
@@ -40,16 +78,50 @@ function update_thread($itemid, $setmention = false) {
 
 	$sql = "";
 
-	foreach ($item AS $field => $data) {
-		if ($sql != "")
-			$sql .= ", ";
+	foreach ($item AS $field => $data)
+		if ($field != "uri") {
+			if ($sql != "")
+				$sql .= ", ";
 
-		$sql .= "`".$field."` = '".$data."'";
-	}
+			$sql .= "`".$field."` = '".dbesc($data)."'";
+		}
 
-	$result = q("UPDATE `thread` SET ".$sql." WHERE `iid` = %d", $itemid);
+	$result = q("UPDATE `thread` SET ".$sql." WHERE `iid` = %d", intval($itemid));
 
 	logger("update_thread: Update thread for item ".$itemid." - ".print_r($result, true)." ".print_r($item, true), LOGGER_DEBUG);
+
+	// Updating a shadow item entry
+	$items = q("SELECT `id`, `title`, `body`, `created`, `edited`, `commented`, `received`, `changed`, `wall`, `private`, `pubmail`,
+			`moderated`, `visible`, `spam`, `starred`, `bookmark`, `deleted`, `origin`, `forum_mode`, `network`
+			FROM `item` WHERE `uri` = '%s' AND `uid` = 0 LIMIT 1", dbesc($item["uri"]));
+
+	if (!$items)
+		return;
+
+	$item = $items[0];
+
+	$result = q("UPDATE `item` SET `title` = '%s', `body` = '%s', `network` = '%s' WHERE `id` = %d",
+			dbesc($item["title"]),
+			dbesc($item["body"]),
+			dbesc($item["network"]),
+			intval($item["id"])
+		);
+	logger("update_thread: Updating public shadow for post ".$item["id"]." Result: ".print_r($result, true), LOGGER_DEBUG);
+
+	/*
+	$sql = "";
+
+	foreach ($item AS $field => $data)
+		if ($field != "id") {
+			if ($sql != "")
+				$sql .= ", ";
+
+			$sql .= "`".$field."` = '".dbesc($data)."'";
+		}
+	//logger("update_thread: Updating public shadow for post ".$item["id"]." SQL: ".$sql, LOGGER_DEBUG);
+	$result = q("UPDATE `item` SET ".$sql." WHERE `id` = %d", intval($item["id"]));
+	logger("update_thread: Updating public shadow for post ".$item["id"]." Result: ".print_r($result, true), LOGGER_DEBUG);
+	*/
 }
 
 function delete_thread_uri($itemuri, $uid) {
@@ -61,13 +133,28 @@ function delete_thread_uri($itemuri, $uid) {
 }
 
 function delete_thread($itemid) {
+	$item = q("SELECT `uri`, `uid` FROM `thread` WHERE `iid` = %d", intval($itemid));
+
 	$result = q("DELETE FROM `thread` WHERE `iid` = %d", intval($itemid));
 
 	logger("delete_thread: Deleted thread for item ".$itemid." - ".print_r($result, true), LOGGER_DEBUG);
+
+	if ($count($item)) {
+		$r = q("SELECT `id` FROM `item` WHERE `uri` = '%s' AND NOT (`uid` IN (%d, 0))",
+				dbesc($item["uri"]),
+				intval($item["uid"])
+			);
+		if (!count($r)) {
+			$r = q("DELETE FROM `item` WHERE `uri` = '%s' AND `uid` = 0)",
+				dbesc($item["uri"])
+			);
+			logger("delete_thread: Deleted shadow for item ".$item["uri"]." - ".print_r($result, true), LOGGER_DEBUG);
+		}
+	}
 }
 
 function update_threads() {
-        global $db;
+	global $db;
 
 	logger("update_threads: start");
 
