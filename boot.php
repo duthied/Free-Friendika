70ba1667e10cc9b8350d32c0c788e780eb2f50cc
Michael Vogel <icarus@dabo.de>
2014-11-08 00:07:21 +0100

    It is now possible to define the hostname from the admin page. This should definetely end the problem that the hostname couldn't be detected unter certain conditions.

--- boot.php
+++ boot.php
@@ -522,28 +522,28 @@ if(! class_exists('App')) {
 				if (substr($this->query_string, 0, 1) == "/")
 					$this->query_string = substr($this->query_string, 1);
 			}
-                        
+
 			if (x($_GET,'pagename'))
 				$this->cmd = trim($_GET['pagename'],'/\\');
 			elseif (x($_GET,'q'))
 				$this->cmd = trim($_GET['q'],'/\\');
-                            
-                        
+
+
                         // fix query_string
                         $this->query_string = str_replace($this->cmd."&",$this->cmd."?", $this->query_string);
-                        
-                        
+
+
 			// unix style "homedir"
-                        
+
 			if(substr($this->cmd,0,1) === '~')
                         	$this->cmd = 'profile/' . substr($this->cmd,1);
-                        
+
 			// Diaspora style profile url
 
 			if(substr($this->cmd,0,2) === 'u/')
 				$this->cmd = 'profile/' . substr($this->cmd,2);
 
-                                
+
 			/**
 			 *
 			 * Break the URL path into C style argc/argv style arguments for our
@@ -639,6 +639,9 @@ if(! class_exists('App')) {
 				}
 			}
 
+			if (get_config('config','hostname') != "")
+				$this->hostname = get_config('config','hostname');
+
 			$this->baseurl = $scheme . "://" . $this->hostname . ((isset($this->path) && strlen($this->path)) ? '/' . $this->path : '' );
 			return $this->baseurl;
 		}
@@ -660,12 +663,19 @@ if(! class_exists('App')) {
 				if (file_exists(".htpreconfig.php"))
 					@include(".htpreconfig.php");
 
-				$this->hostname = $hostname;
+				if (get_config('config','hostname') != "")
+					$this->hostname = get_config('config','hostname');
+
+				if (!isset($this->hostname) OR ($this->hostname == ""))
+					$this->hostname = $hostname;
 			}
 
 		}
 
 		function get_hostname() {
+			if (get_config('config','hostname') != "")
+				$this->hostname = get_config('config','hostname');
+
 			return $this->hostname;
 		}
 
