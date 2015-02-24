6a5bb80c824dd74f0473facc5db490e7f400a931
Fabrixxm <fabrix.xm@gmail.com>
2015-02-24 09:00:37 +0100

    Friendica 3.3.3 See CHANGELOG

--- boot.php
+++ boot.php
@@ -16,7 +16,7 @@ require_once('include/dbstructure.php');
 
 define ( 'FRIENDICA_PLATFORM',     'Friendica');
 define ( 'FRIENDICA_CODENAME',     'Ginger');
-define ( 'FRIENDICA_VERSION',      '3.3.3-RC' );
+define ( 'FRIENDICA_VERSION',      '3.3.3' );
 define ( 'DFRN_PROTOCOL_VERSION',  '2.23'    );
 define ( 'DB_UPDATE_VERSION',      1178      );
 define ( 'EOL',                    "<br />\r\n"     );
