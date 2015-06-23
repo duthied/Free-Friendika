600c9c84742092ae89e24d2ffbbdff392f3f72ed
Fabrixxm <fabrix.xm@gmail.com>
2015-06-23 10:51:29 +0200

    remove RINO from htconfig.php

--- htconfig.php
+++ htconfig.php
@@ -61,12 +61,6 @@ $a->config['system']['directory_search_url'] = 'http://dir.friendica.com/directo
 
 $a->config['system']['huburl'] = '[internal]';
 
-// Server-to-server private message encryption (RINO) is allowed by default. 
-// Encryption will only be provided if this setting is true and the
-// PHP mcrypt extension is installed on both systems 
-
-$a->config['system']['rino_encrypt'] = true;
-
 // allowed themes (change this from admin panel after installation)
 
 $a->config['system']['allowed_themes'] = 'dispy,quattro,vier,darkzero,duepuntozero,greenzero,purplezero,slackr,diabook';
