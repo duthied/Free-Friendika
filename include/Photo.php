68e565889d2e019b67d4e00a03076ec042bcf5ff
rabuzarus <>
2015-06-29 11:22:29 +0200

    moved formatBytes() to /include/text.php

--- include/Photo.php
+++ include/Photo.php
@@ -1014,14 +1014,3 @@ function store_photo($a, $uid, $imagedata = "", $url = "") {
 	return($image);
 }
 
-function formatBytes($bytes, $precision = 2) { 
-	 $units = array('B', 'KB', 'MB', 'GB', 'TB'); 
-
-	$bytes = max($bytes, 0); 
-	$pow = floor(($bytes ? log($bytes) : 0) / log(1024)); 
-	$pow = min($pow, count($units) - 1); 
-
-	$bytes /= pow(1024, $pow);
-
-	return round($bytes, $precision) . ' ' . $units[$pow]; 
-} 
