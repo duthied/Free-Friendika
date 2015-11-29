91011b71903a059d65b9eb9cc62c3e73fbfe3c7a
Michael Vogel <icarus@dabo.de>
2015-11-29 23:36:19 +0100

    Removing of useless code

--- mod/contacts.php
+++ mod/contacts.php
@@ -55,8 +55,7 @@ function contacts_init(&$a) {
 		$findpeople_widget .= findpeople_widget();
 	}
 
-	if ($a->argv[2] != "posts")
-		$groups_widget .= group_side('contacts','group','full',0,$contact_id);
+	$groups_widget .= group_side('contacts','group','full',0,$contact_id);
 
 	$a->page['aside'] .= replace_macros(get_markup_template("contacts-widget-sidebar.tpl"),array(
 		'$vcard_widget' => $vcard_widget,
