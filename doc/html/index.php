b31c602e9b8fa580ee0e6a8afb9de1c761bf122c
rabuzarus <>
2015-12-03 00:06:11 +0100

    added code reference to docs

---
+++ doc/html/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>$Projectname Doxygen API Documentation</title>
+</head>
+<body>
+<h1>$Projectname Doxygen API Documentation not rendered</h1>
+
+To get the Doxygen API Documentation you must render it with the program <a href="http://www.doxygen.org">Doxygen</a> (included in most distributions).
+<pre>
+$ doxygen util/Doxyfile
+</pre>
+<br>
+<a href="javascript:history.back()">back</a>
+</body>
+</html>
