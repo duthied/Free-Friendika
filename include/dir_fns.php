aee94b38c174f0e182d6d14309ef3f4aef2563b0
rabuzarus <>
2016-02-01 18:21:29 +0100

    rework autocomplete: add class dir and static function

--- include/dir_fns.php
+++ include/dir_fns.php
@@ -2,45 +2,51 @@
 
 /**
  * @file include/dir_fns.php
- * @brief Functions for directory
  */
+
 
 /**
- * @brief Search global contact table by nick or name
- *  * 
- * @param string $search
- * @return array
+ * @brief This class handels directory related functions
  */
-function dirsearch_global_by_name($search) {
+class dir {
 
-	if($search) {
-		// check supported networks
-		if (get_config('system','diaspora_enabled'))
-			$diaspora = NETWORK_DIASPORA;
-		else
-			$diaspora = NETWORK_DFRN;
+	/**
+	 * @brief Search global contact table by nick or name
+	 *  * 
+	 * @param string $search Name or nick
+	 * @return array
+	 */
+	public static function global_search_by_name($search) {
 
-		if (!get_config('system','ostatus_disabled'))
-			$ostatus = NETWORK_OSTATUS;
-		else
-			$ostatus = NETWORK_DFRN;
+		if($search) {
+			// check supported networks
+			if (get_config('system','diaspora_enabled'))
+				$diaspora = NETWORK_DIASPORA;
+			else
+				$diaspora = NETWORK_DFRN;
 
-		$results = q("SELECT `contact`.`id` AS `cid`, `gcontact`.`url`, `gcontact`.`name`, `gcontact`.`nick`, `gcontact`.`photo`,
-						`gcontact`.`network`, `gcontact`.`keywords`, `gcontact`.`addr`
-					FROM `gcontact`
-					LEFT JOIN `contact` ON `contact`.`nurl` = `gcontact`.`nurl`
-						AND `contact`.`uid` = %d AND NOT `contact`.`blocked`
-						AND NOT `contact`.`pending` AND `contact`.`rel` IN ('%s', '%s')
-					WHERE (`contact`.`id` > 0 OR (NOT `gcontact`.`hide` AND `gcontact`.`network` IN ('%s', '%s', '%s') AND
-					((`gcontact`.`last_contact` >= `gcontact`.`last_failure`) OR (`gcontact`.`updated` >= `gcontact`.`last_failure`)))) AND
-					(`gcontact`.`url` REGEXP '%s' OR `gcontact`.`name` REGEXP '%s' OR `gcontact`.`nick` REGEXP '%s'
-						) 
-						GROUP BY `gcontact`.`nurl`
-						ORDER BY `gcontact`.`updated` DESC ",
-					intval(local_user()), dbesc(CONTACT_IS_SHARING), dbesc(CONTACT_IS_FRIEND),
-					dbesc(NETWORK_DFRN), dbesc($ostatus), dbesc($diaspora),
-					dbesc(escape_tags($search)), dbesc(escape_tags($search)), dbesc(escape_tags($search)));
-		return $results;
+			if (!get_config('system','ostatus_disabled'))
+				$ostatus = NETWORK_OSTATUS;
+			else
+				$ostatus = NETWORK_DFRN;
+
+			$results = q("SELECT `contact`.`id` AS `cid`, `gcontact`.`url`, `gcontact`.`name`, `gcontact`.`nick`, `gcontact`.`photo`,
+							`gcontact`.`network`, `gcontact`.`keywords`, `gcontact`.`addr`
+						FROM `gcontact`
+						LEFT JOIN `contact` ON `contact`.`nurl` = `gcontact`.`nurl`
+							AND `contact`.`uid` = %d AND NOT `contact`.`blocked`
+							AND NOT `contact`.`pending` AND `contact`.`rel` IN ('%s', '%s')
+						WHERE (`contact`.`id` > 0 OR (NOT `gcontact`.`hide` AND `gcontact`.`network` IN ('%s', '%s', '%s') AND
+						((`gcontact`.`last_contact` >= `gcontact`.`last_failure`) OR (`gcontact`.`updated` >= `gcontact`.`last_failure`)))) AND
+						(`gcontact`.`url` REGEXP '%s' OR `gcontact`.`name` REGEXP '%s' OR `gcontact`.`nick` REGEXP '%s'
+							) 
+							GROUP BY `gcontact`.`nurl`
+							ORDER BY `gcontact`.`updated` DESC ",
+						intval(local_user()), dbesc(CONTACT_IS_SHARING), dbesc(CONTACT_IS_FRIEND),
+						dbesc(NETWORK_DFRN), dbesc($ostatus), dbesc($diaspora),
+						dbesc(escape_tags($search)), dbesc(escape_tags($search)), dbesc(escape_tags($search)));
+			return $results;
+		}
+
 	}
-		
 }
--- view/templates/contacts-head.tpl
+++ view/templates/contacts-head.tpl
@@ -2,18 +2,6 @@
 <script>
 $(document).ready(function() {
 	$("#contacts-search").contact_autocomplete(baseurl + '/acl', 'a', true);
- 
-
-	$("#contacts-search").keyup(function(event){
-		if(event.keyCode == 13){
-			$("#contacts-search").click();
-		}
-	});
-	$(".autocomplete-w1 .selected").keyup(function(event){
-		if(event.keyCode == 13){
-			$("#contacts-search").click();
-		}
-	});
 });
 </script>
 
