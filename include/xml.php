265af9c99b450a1fe3a4160f50f1e495f78f9ac6
Michael Vogel <icarus@dabo.de>
2016-03-06 00:53:30 +0100

    Receiving should be complete, sending partially works

---
+++ include/xml.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * @brief This class contain functions to work with XML data
+ *
+ */
+class xml {
+	function from_array($array, &$xml) {
+
+		if (!is_object($xml)) {
+			foreach($array as $key => $value) {
+				$root = new SimpleXMLElement("<".$key."/>");
+				self::from_array($value, $root);
+
+				$dom = dom_import_simplexml($root)->ownerDocument;
+				$dom->formatOutput = true;
+				$xml = $dom;
+				return $dom->saveXML();
+			}
+		}
+
+		foreach($array as $key => $value) {
+			if (!is_array($value) AND !is_numeric($key))
+				$xml->addChild($key, $value);
+			elseif (is_array($value))
+				self::from_array($value, $xml->addChild($key));
+		}
+	}
+
+	function copy(&$source, &$target, $elementname) {
+		if (count($source->children()) == 0)
+			$target->addChild($elementname, $source);
+		else {
+			$child = $target->addChild($elementname);
+			foreach ($source->children() AS $childfield => $childentry)
+				self::copy($childentry, $child, $childfield);
+		}
+	}
+}
+?>
