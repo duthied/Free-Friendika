f282748c52f609158ab54123abb77713616baf4e
Michael Vogel <icarus@dabo.de>
2016-03-17 23:44:18 +0100

    Added some documentation, script to generate a basic doxygen header

---
+++ util/createdoxygen.php
@@ -0,0 +1,82 @@
+#!/usr/bin/php
+<?php
+if (count($_SERVER["argv"]) < 2)
+	die("usage: createdoxygen.php file\n");
+
+$file = $_SERVER["argv"][1];
+$data = file_get_contents($file);
+
+$lines = explode("\n", $data);
+
+$previous = "";
+
+foreach ($lines AS $line) {
+	$line = rtrim(trim($line, "\r"));
+
+	if (strstr(strtolower($line), "function")) {
+		$detect = strtolower(trim($line));
+		$detect = implode(" ", explode(" ", $detect));
+
+		$found = false;
+
+		if (substr($detect, 0, 9) == "function ")
+			$found = true;
+
+		if (substr($detect, 0, 17) == "private function ")
+			$found = true;
+
+		if (substr($detect, 0, 23) == "public static function ")
+			$found = true;
+
+		if (substr($detect, 0, 10) == "function (")
+			$found = false;
+
+		if ($found and (trim($previous) == "*/"))
+			$found = false;
+
+		if ($found and !strstr($detect, "{"))
+			$found = false;
+
+		if ($found) {
+			echo add_documentation($line);
+		}
+	}
+	echo $line."\n";
+	$previous = $line;
+}
+
+function add_documentation($line) {
+
+	$trimmed = ltrim($line);
+	$length = strlen($line) - strlen($trimmed);
+	$space = substr($line, 0, $length);
+
+	$block = $space."/**\n".
+		$space." * @brief \n".
+		$space." *\n"; /**/
+
+
+	$left = strpos($line, "(");
+	$line = substr($line, $left + 1);
+
+	$right = strpos($line, ")");
+	$line = trim(substr($line, 0, $right));
+
+	if ($line != "") {
+		$parameters = explode(",", $line);
+		foreach ($parameters AS $parameter) {
+			$parameter = trim($parameter);
+			$splitted = explode("=", $parameter);
+
+			$block .= $space." * @param ".trim($splitted[0], "& ")."\n";
+		}
+		if (count($parameters) > 0)
+			$block .= $space." *\n";
+	}
+
+	$block .= $space." * @return \n".
+		$space." */\n";
+
+	return $block;
+}
+?>
