f9c9019d90733be6743fbdce2005e0ae515b123c
rabuzarus <>
2016-04-16 01:00:23 +0200

    move js from theme.php to theme.js

--- theme.php
+++ theme.php
@@ -27,59 +27,6 @@ function frio_init(&$a) {
 
 	if ($style == "")
 		$style = get_config('frio', 'style');
-
-$a->page['htmlhead'] .= <<< EOT
-<script type="text/javascript">
-
-function insertFormatting(comment,BBcode,id) {
-
-		var tmpStr = $("#comment-edit-text-" + id).val();
-		if(tmpStr == comment) {
-			tmpStr = "";
-			$("#comment-edit-text-" + id).addClass("comment-edit-text-full");
-			$("#comment-edit-text-" + id).removeClass("comment-edit-text-empty");
-			openMenu("comment-edit-submit-wrapper-" + id);
-			$("#comment-edit-text-" + id).val(tmpStr);
-		}
-
-	textarea = document.getElementById("comment-edit-text-" +id);
-	if (document.selection) {
-		textarea.focus();
-		selected = document.selection.createRange();
-		if (BBcode == "url"){
-			selected.text = "["+BBcode+"]" + "http://" +  selected.text + "[/"+BBcode+"]";
-			} else
-		selected.text = "["+BBcode+"]" + selected.text + "[/"+BBcode+"]";
-	} else if (textarea.selectionStart || textarea.selectionStart == "0") {
-		var start = textarea.selectionStart;
-		var end = textarea.selectionEnd;
-		if (BBcode == "url"){
-			textarea.value = textarea.value.substring(0, start) + "["+BBcode+"]" + "http://" + textarea.value.substring(start, end) + "[/"+BBcode+"]" + textarea.value.substring(end, textarea.value.length);
-			} else
-		textarea.value = textarea.value.substring(0, start) + "["+BBcode+"]" + textarea.value.substring(start, end) + "[/"+BBcode+"]" + textarea.value.substring(end, textarea.value.length);
-	}
-	return true;
-}
-
-
-function showThread(id) {
-	$("#collapsed-comments-" + id).show()
-	$("#collapsed-comments-" + id + " .collapsed-comments").show()
-}
-function hideThread(id) {
-	$("#collapsed-comments-" + id).hide()
-	$("#collapsed-comments-" + id + " .collapsed-comments").hide()
-}
-
-
-function cmtBbOpen(id) {
-	$("#comment-edit-bb-" + id).show();
-}
-function cmtBbClose(id) {
-	$("#comment-edit-bb-" + id).hide();
-}
-</script>
-EOT;
 }
 
 function frio_install() {
