3d27ebcb341dd2c1ed8613469362937c51727d5e
Michael Vogel <icarus@dabo.de>
2016-06-06 06:29:03 +0200

    Added documentation

--- include/dbm.php
+++ include/dbm.php
@@ -1,5 +1,16 @@
 <?php
+/**
+ * @brief This class contain functions for the database management
+ *
+ */
 class dbm {
+	/**
+	 * @brief Return a list of database processes
+	 *
+	 * @return array
+	 *      'list' => List of processes, separated in their different states
+	 *      'amount' => Number of concurrent database processes
+	 */
 	public static function processlist() {
 		$r = q("SHOW PROCESSLIST");
 		$s = array();
@@ -8,14 +19,13 @@ class dbm {
 		$states = array();
 		foreach ($r AS $process) {
 			$state = trim($process["State"]);
+
+			// Filter out all idle processes
 			if (!in_array($state, array("", "init", "statistics"))) {
 				++$states[$state];
 				++$processes;
 			}
 		}
-		// query end
-		// Sending data
-		// updating
 
 		$statelist = "";
 		foreach ($states AS $state => $usage) {
