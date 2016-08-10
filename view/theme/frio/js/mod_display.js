5e440e5359ce77f6fc9b902890876b624e599518
rabuzarus <rabuzarus@t-online.de>
2016-08-10 14:26:16 +0200

    frio: highlight - wait until the whole page is loaded

--- view/theme/frio/js/mod_display.js
+++ view/theme/frio/js/mod_display.js
@@ -5,7 +5,7 @@
 // Catch the GUID from the URL
 var itemGuid = window.location.pathname.split("/").pop();
 
-$(document).ready(function(){
+$(window).load(function(){
 	// Scroll to the Item by its GUID
 	scrollToItem('item-'+itemGuid);
 });
