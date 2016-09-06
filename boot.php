0a50dbaece795fe80b9b115facc0d1f7498351d1
Michael Vogel <ike@pirati.ca>
2016-09-06 19:01:05 +0000

    New database version

--- boot.php
+++ boot.php
@@ -38,7 +38,7 @@ define ( 'FRIENDICA_PLATFORM',     'Friendica');
 define ( 'FRIENDICA_CODENAME',     'Asparagus');
 define ( 'FRIENDICA_VERSION',      '3.5-dev' );
 define ( 'DFRN_PROTOCOL_VERSION',  '2.23'    );
-define ( 'DB_UPDATE_VERSION',      1200      );
+define ( 'DB_UPDATE_VERSION',      1201      );
 
 /**
  * @brief Constant with a HTML line break.
