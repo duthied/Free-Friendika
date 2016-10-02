08a30bc1c6c68d81895e0d8c8905680c7d26221f
Hypolite Petovan <ben.lort@gmail.com>
2016-10-02 15:23:05 -0400

    Fix Issue #2822 - Update update_*.php files with new bandwitdh_saver user setting
    - Fix formatting according to PSR-2
    - Add bandwitdh_saver setting check before replacing with "Embedded
    content - Please reload"

--- mod/update_community.php
+++ mod/update_community.php
@@ -2,20 +2,21 @@
 
 // See update_profile.php for documentation
 
-require_once('mod/community.php');
+require_once 'mod/community.php';
 
-function update_community_content(&$a) {
+function update_community_content(&$a)
+{
+    header('Content-type: text/html');
+    echo "<!DOCTYPE html><html><body>\r\n";
+    echo '<section>';
 
-	header("Content-type: text/html");
-	echo "<!DOCTYPE html><html><body>\r\n";
-	echo "<section>";
+    $text = community_content($a, true);
+    $pattern = '/<img([^>]*) src="([^"]*)"/';
+    $replace = '<img${1} dst="${2}"';
+    $text = preg_replace($pattern, $replace, $text);
 
-        $text = community_content($a,true);
-        $pattern = "/<img([^>]*) src=\"([^\"]*)\"/";
-        $replace = "<img\${1} dst=\"\${2}\"";
-        $text = preg_replace($pattern, $replace, $text);
-
-		$replace = '<br />' . t('[Embedded content - reload page to view]') . '<br />';
+    if (get_pconfig(local_user(), 'system', 'bandwith_saver')) {
+        $replace = '<br />'.t('[Embedded content - reload page to view]').'<br />';
         $pattern = "/<\s*audio[^>]*>(.*?)<\s*\/\s*audio>/i";
         $text = preg_replace($pattern, $replace, $text);
         $pattern = "/<\s*video[^>]*>(.*?)<\s*\/\s*video>/i";
@@ -24,10 +25,11 @@ function update_community_content(&$a) {
         $text = preg_replace($pattern, $replace, $text);
         $pattern = "/<\s*iframe[^>]*>(.*?)<\s*\/\s*iframe>/i";
         $text = preg_replace($pattern, $replace, $text);
+    }
 
-        echo str_replace("\t",'       ',$text);
-	echo "</section>";
-	echo "</body></html>\r\n";
-	killme();
-
+    // reportedly some versions of MSIE don't handle tabs in XMLHttpRequest documents very well
+    echo str_replace("\t", '       ', $text);
+    echo '</section>';
+    echo "</body></html>\r\n";
+    killme();
 }
