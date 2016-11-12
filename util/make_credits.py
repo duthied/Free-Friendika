74912a3b2483e5355328b0212dffdb7d213bff52
Tobias Diekershoff <tobias.diekershoff@gmx.net>
2016-11-12 12:49:31 +0100

    double contributor name filtered out for the credits generator

--- util/make_credits.py
+++ util/make_credits.py
@@ -23,7 +23,7 @@ import os, glob, subprocess
 #  not work in some cases.
 dontinclude = ['root', 'friendica', 'bavatar', 'tony baldwin', 'Taek', 'silke m',
                'leberwurscht', 'abinoam', 'fabrixxm', 'FULL NAME', 'Hauke Zuehl',
-               'Michal Supler', 'michal_s', 'Manuel Pérez', 'rabuzarus']
+               'Michal Supler', 'michal_s', 'Manuel Pérez', 'rabuzarus', 'Alberto Díaz']
 
 
 #  this script is in the /util sub-directory of the friendica installation
