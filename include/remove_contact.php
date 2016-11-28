eede89f53792bdebbddd6cb95ef558ced68afee2
Michael <heluecht@pirati.ca>
2016-11-28 22:18:37 +0000

    Some brackets added

--- include/remove_contact.php
+++ include/remove_contact.php
@@ -8,8 +8,9 @@ require_once("boot.php");
 function remove_contact_run($argv, $argc) {
 	global $a, $db;
 
-	if (is_null($a))
+	if (is_null($a)) {
 		$a = new App;
+	}
 
 	if (is_null($db)) {
 		@include(".htconfig.php");
