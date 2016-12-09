a564e31451238cc66efc90d7a4f9c79c0317514f
Tobias Diekershoff <tobias.diekershoff@gmx.net>
2016-12-09 16:43:40 +0100

    use %placeholder% instead of Friendica

--- util/convert_innodb.sql
+++ util/convert_innodb.sql
@@ -1,4 +1,6 @@
-
+#  script to convert tables from MyISAM to InnoDB
+#  change the %PLACEHOLDER% to ythe actual name of your Friendica DB
+ 
 SELECT CONCAT('ALTER TABLE ',table_schema,'.',table_name,' engine=InnoDB;') 
 FROM information_schema.tables 
-WHERE engine = 'MyISAM' AND  `table_schema` = 'friendica';
+WHERE engine = 'MyISAM' AND  `table_schema` = '%PLACEHOLDER%';
