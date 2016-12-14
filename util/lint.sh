7f1da5b1a9b3cc63dbc158d1e18b26f09e5c9865
Roland Häder <roland@mxchange.org>
2016-12-14 12:45:11 +0100

    added lint.sh which needs to be executed like this:
    $ ./util/lint.sh
    
    No parameters needed. It will use "php -l" (current interpreter) for checking
    for syntax errors in all PHP scripts.
    
    Signed-off-by: Roland Häder <roland@mxchange.org>

---
+++ util/lint.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+if ! test -e "index.php"; then
+	echo "$0: Please execute this script from root directory."
+	exit 1
+fi
+
+echo "$0: Analysing PHP scripts for syntax errors (lint) ..."
+LINT=`find -type f -name "*.php" -exec php -l -f {} 2>&1 \; | grep -v "No syntax errors detected in" | grep -v "FUSE_EDEADLK" | sort --unique`
+
+if test "${LINT}" != ""; then
+	echo "${LINT}"
+else
+	echo "$0: No syntax errors found."
+fi
