e06cac420b0eb92c04d5cc74f65bc542b217c24f
Roland Häder <roland@mxchange.org>
2016-12-14 12:51:20 +0100

    Use [] instead of test.
    Signed-off-by: Roland Häder <roland@mxchange.org>

--- util/lint.sh
+++ util/lint.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-if ! test -e "index.php"; then
+if [ ! -e "index.php" ]
+then
 	echo "$0: Please execute this script from root directory."
 	exit 1
 fi
@@ -8,7 +9,8 @@ fi
 echo "$0: Analysing PHP scripts for syntax errors (lint) ..."
 LINT=`find -type f -name "*.php" -exec php -l -f {} 2>&1 \; | grep -v "No syntax errors detected in" | grep -v "FUSE_EDEADLK" | sort --unique`
 
-if test "${LINT}" != ""; then
+if [ -n "${LINT}" ]
+then
 	echo "${LINT}"
 else
 	echo "$0: No syntax errors found."
