54905a3d81065915418af7f96953f957bf795300
Roland Häder <roland@mxchange.org>
2016-12-20 17:43:46 +0100

    added more curly braces + space between "if" and brace
    Signed-off-by: Roland Häder <roland@mxchange.org>

--- mod/subthread.php
+++ mod/subthread.php
@@ -86,14 +86,7 @@ function subthread_content(App &$a) {
 	$uri = item_new_uri($a->get_hostname(),$owner_uid);
 
 	$post_type = (($item['resource-id']) ? t('photo') : t('status'));
-<<<<<<< upstream/develop
 	$objtype = (($item['resource-id']) ? ACTIVITY_OBJ_IMAGE : ACTIVITY_OBJ_NOTE );
-=======
-	$objtype = (($item['resource-id']) ? ACTIVITY_OBJ_PHOTO : ACTIVITY_OBJ_NOTE );
-<<<<<<< upstream/develop
->>>>>>> HEAD~33
-=======
->>>>>>> HEAD~32
 	$link = xmlify('<link rel="alternate" type="text/html" href="' . App::get_baseurl() . '/display/' . $owner['nickname'] . '/' . $item['id'] . '" />' . "\n") ;
 	$body = $item['body'];
 
