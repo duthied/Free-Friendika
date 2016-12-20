40bdc5d33c740ed5c73f56056457a0e7b2d82117
Roland Häder <roland@mxchange.org>
2016-12-20 11:14:30 +0100

    added curly braces + space between "if" and brace + added TODO
    Signed-off-by: Roland Häder <roland@mxchange.org>

--- mod/home.php
+++ mod/home.php
@@ -6,12 +6,13 @@ function home_init(App &$a) {
 	$ret = array();
 	call_hooks('home_init',$ret);
 
-	if(local_user() && ($a->user['nickname']))
+	if (local_user() && ($a->user['nickname'])) {
 		goaway(App::get_baseurl()."/network");
-		//goaway(App::get_baseurl()."/profile/".$a->user['nickname']);
+	}
 
-	if(strlen(get_config('system','singleuser')))
+	if (strlen(get_config('system','singleuser'))) {
 		goaway(App::get_baseurl()."/profile/" . get_config('system','singleuser'));
+	}
 
 }}
 
@@ -21,18 +22,24 @@ function home_content(App &$a) {
 
 	$o = '';
 
-	if(x($_SESSION,'theme'))
+	if (x($_SESSION,'theme')) {
 		unset($_SESSION['theme']);
-	if(x($_SESSION,'mobile-theme'))
+	}
+	if (x($_SESSION,'mobile-theme')) {
 		unset($_SESSION['mobile-theme']);
+	}
 
-	if(file_exists('home.html')){
-		if(file_exists('home.css')){
-			  $a->page['htmlhead'] .= '<link rel="stylesheet" type="text/css" href="'.App::get_baseurl().'/home.css'.'" media="all" />';}
+	/// @TODO No absolute path used, maybe risky (security)
+	if (file_exists('home.html')) {
+		if (file_exists('home.css')) {
+			$a->page['htmlhead'] .= '<link rel="stylesheet" type="text/css" href="'.App::get_baseurl().'/home.css'.'" media="all" />';
+		}
 
- 		$o .= file_get_contents('home.html');}
+		$o .= file_get_contents('home.html');}
 
-	else 	$o .= '<h1>'.((x($a->config,'sitename')) ? sprintf(t("Welcome to %s"), $a->config['sitename']) : "").'</h1>';
+	else {
+		$o .= '<h1>'.((x($a->config,'sitename')) ? sprintf(t("Welcome to %s"), $a->config['sitename']) : "").'</h1>';
+	}
 
 
 	$o .= login(($a->config['register_policy'] == REGISTER_CLOSED) ? 0 : 1);
