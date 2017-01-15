99e9791d1ff2737a1b15e28800cc664968674cb8
Michael <heluecht@pirati.ca>
2017-01-15 15:06:32 +0000

    We fetched a little bit too much ...

--- mod/fetch.php
+++ mod/fetch.php
@@ -42,7 +42,7 @@ function fetch_init(App $a) {
 
 	// Fetch some data from the author (We could combine both queries - but I think this is more readable)
 	$r = q("SELECT `user`.`prvkey`, `contact`.`addr`, `user`.`nickname`, `contact`.`nick` FROM `user`
-		INNER JOIN `contact` ON `contact`.`uid` = `user`.`uid`
+		INNER JOIN `contact` ON `contact`.`uid` = `user`.`uid` AND `contact`.`self`
 		WHERE `user`.`uid` = %d", intval($item[0]["uid"]));
 	if (!$r) {
 		header($_SERVER["SERVER_PROTOCOL"].' 404 '.t('Not Found'));
