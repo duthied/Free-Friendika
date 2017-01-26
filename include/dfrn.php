bf0a07b7ae85ce0535e6198e4f73857d8c184976
Roland Häder <roland@mxchange.org>
2017-01-26 09:38:52 +0100

    Continued: - added missing space/curly braces - added TODOs for later adding a lot type-hints, without these (and they are   long time around in PHP) anything can be handled over to the method/function.
    Signed-off-by: Roland Häder <roland@mxchange.org>

--- include/dfrn.php
+++ include/dfrn.php
@@ -1277,7 +1277,7 @@ class dfrn {
 			$href = "";
 			$width = 0;
 			foreach ($avatar->attributes AS $attributes) {
-				/// @TODO Rewrite these similar if () to one switch
+				/// @TODO Rewrite these similar if() to one switch
 				if ($attributes->name == "href") {
 					$href = $attributes->textContent;
 				}
@@ -2122,7 +2122,7 @@ class dfrn {
 		$title = "";
 		foreach ($links AS $link) {
 			foreach ($link->attributes AS $attributes) {
-				/// @TODO Rewrite these repeated (same) if () statements to a switch()
+				/// @TODO Rewrite these repeated (same) if() statements to a switch()
 				if ($attributes->name == "href") {
 					$href = $attributes->textContent;
 				}
