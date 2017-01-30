dae125e8ee20b997d166e88a559e29667fbbdd2e
Michael <heluecht@pirati.ca>
2017-01-30 21:18:08 +0000

    Issue 3135: dbupdate is started again and again

--- include/dbupdate.php
+++ include/dbupdate.php
@@ -20,6 +20,10 @@ function dbupdate_run(&$argv, &$argc) {
 
 	Config::load();
 
+	// We are deleting the latest dbupdate entry.
+	// This is done to avoid endless loops because the update was interupted.
+	Config::delete('database','dbupdate_'.DB_UPDATE_VERSION);
+
 	update_db($a);
 }
 
