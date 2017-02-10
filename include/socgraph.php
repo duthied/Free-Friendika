f5a634abbcad08131abbefa8442f622ba1b40b14
Michael <heluecht@pirati.ca>
2017-02-10 19:23:46 +0000

    Changed server detection.

--- include/socgraph.php
+++ include/socgraph.php
@@ -765,7 +765,7 @@ function poco_check_server($server_url, $network = "", $force = false) {
 
 	if (!$serverret["success"] OR ($serverret["body"] == "") OR (sizeof($xmlobj) == 0) OR !is_object($xmlobj)) {
 		// Workaround for bad configured servers (known nginx problem)
-		if ($serverret["debug"]["http_code"] != "403") {
+		if (!in_array($serverret["debug"]["http_code"], array("403", "404"))) {
 			$last_failure = datetime_convert();
 			$failure = true;
 		}
