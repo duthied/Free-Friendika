8dd732a89695c33952130780d4b646798ba3fde2
Michael <heluecht@pirati.ca>
2017-03-19 13:24:07 +0000

    Issue 3242: Define a maintenance reason

--- util/maintenance.php
+++ util/maintenance.php
@@ -17,17 +17,37 @@ unset($db_host, $db_user, $db_pass, $db_data);
 Config::load();
 
 $maint_mode = 1;
-if($argc > 1)
+if($argc > 1) {
 	$maint_mode = intval($argv[1]);
-set_config('system', 'maintenance', $maint_mode);
+}
 
-if($maint_mode)
+Config::set('system', 'maintenance', $maint_mode);
+
+if($maint_mode AND ($argc > 2)) {
+	$reason_arr = $argv;
+	array_shift($reason_arr);
+	array_shift($reason_arr);
+
+	$reason = implode(' ', $reason_arr);
+	Config::set('system', 'maintenance_reason', $reason);
+} else {
+	Config::set('system', 'maintenance_reason', '');
+}
+
+if($maint_mode) {
 	$mode_str = "maintenance mode";
-else
+} else {
 	$mode_str = "normal mode";
+}
 
-echo "\n\tSystem set in $mode_str\n\n";
+echo "\n\tSystem set in $mode_str\n";
+
+if ($reason != '') {
+	echo "\tMaintenance reason: $reason\n\n";
+} else {
+	echo "\n";
+}
+
 echo "Usage:\n\n";
-echo "\tphp {$argv[0]} [1]\tSet the system in maintenance mode\n";
+echo "\tphp {$argv[0]} [1] [Maintenance reason]\tSet the system in maintenance mode\n";
 echo "\tphp {$argv[0]} 0  \tSet the system in normal mode\n\n";
-
