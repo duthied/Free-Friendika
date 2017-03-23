a5dba628c44fe64d2f4138dce7ef1cec13148770
Roland Haeder <roland@mxchange.org>
2017-03-23 22:49:05 +0100

    old behaviour restored
    Signed-off-by: Roland Haeder <roland@mxchange.org>

--- src/Protocol/DFRN.php
+++ src/Protocol/DFRN.php
@@ -2360,7 +2360,11 @@ class DFRN
 					);
 
 					if (!DBM::is_result($r)) {
+						/*
+						 * @TODO maybe one day:
 						logger("Query failed to execute, no result returned in " . __FUNCTION__);
+						killme();
+						*/
 						return false;
 					}
 
