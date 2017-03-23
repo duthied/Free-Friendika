116d0ee1e01914aa7783ce92ebc7fa50c5dd11c2
Roland Haeder <roland@mxchange.org>
2017-03-23 22:26:07 +0100

    better comment added by @Hypolite
    Signed-off-by: Roland Haeder <roland@mxchange.org>

--- src/Protocol/DFRN.php
+++ src/Protocol/DFRN.php
@@ -1988,10 +1988,8 @@ class DFRN
 		 */
 		if (!DBM::is_result($r)) {
 			// database record did not get created. Quietly give up.
-			killme();
+			return false;
 		}
-
-		$fid = $r[0]["id"];
 
 		$hash = random_string();
 
