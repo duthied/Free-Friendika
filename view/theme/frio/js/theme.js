0ffe426ff1bf63cf5220c4d0994071c33b3a95eb
fabrixxm <fabrix.xm@gmail.com>
2017-04-27 08:39:35 +0200

    Frio: Enable stiky aside only on desktop-size windows.
    "Hack" to fix the "scroll back to top" problem with
    chrome-based browsers, sticky-kit and off-canvas aside

--- view/theme/frio/js/theme.js
+++ view/theme/frio/js/theme.js
@@ -317,17 +317,19 @@ $(document).ready(function(){
 	});
 
 	/*
-	 * Sticky aside on page scroll
+	 * Sticky aside on page scroll, only on desktop
 	 */
-	$("aside").stick_in_parent({
-		offset_top: 100, // px, header + tab bar + spacing
-		recalc_every: 10
-	});
-	// recalculate sticky aside on clicks.
-	// this handle height changes on expanding submenus
-	$("aside").on("click", function(){
-		$(document.body).trigger("sticky_kit:recalc");
-	});
+	if ($(window).width() > 976) {
+		$("aside").stick_in_parent({
+			offset_top: 100, // px, header + tab bar + spacing
+			recalc_every: 10
+		});
+		// recalculate sticky aside on clicks.
+		// this handle height changes on expanding submenus
+		$("aside").on("click", function(){
+			$(document.body).trigger("sticky_kit:recalc");
+		});
+	}
 });
 
 function openClose(theID) {
