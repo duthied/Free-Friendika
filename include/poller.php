16276b21ebf617c5c2277b0c25f43f74131d5778
Michael <heluecht@pirati.ca>
2017-06-04 19:05:15 +0000

    Typo

--- include/poller.php
+++ include/poller.php
@@ -106,7 +106,7 @@ function poller_run($argv, $argc){
 
 		// Quit the poller once every hour
 		if (time() > ($starttime + 3600)) {
-			logger('Process lifetime reachted, quitting.', LOGGER_DEBUG);
+			logger('Process lifetime reached, quitting.', LOGGER_DEBUG);
 			return;
 		}
 	}
