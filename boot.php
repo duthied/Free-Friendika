cf179188b6b2a341c2d91c253370feab95c7e0a4
Michael <heluecht@pirati.ca>
2017-06-17 13:58:18 +0000

    Some more worker fine tuning

--- boot.php
+++ boot.php
@@ -42,7 +42,7 @@ define ( 'FRIENDICA_PLATFORM',     'Friendica');
 define ( 'FRIENDICA_CODENAME',     'Asparagus');
 define ( 'FRIENDICA_VERSION',      '3.5.3-dev' );
 define ( 'DFRN_PROTOCOL_VERSION',  '2.23'    );
-define ( 'DB_UPDATE_VERSION',      1229      );
+define ( 'DB_UPDATE_VERSION',      1230      );
 
 /**
  * @brief Constant with a HTML line break.
--- update.php
+++ update.php
@@ -1,6 +1,6 @@
 <?php
 
-define('UPDATE_VERSION' , 1229);
+define('UPDATE_VERSION' , 1230);
 
 /**
  *
