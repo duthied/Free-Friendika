29a66c8336cb127f9a927e1c5cd317f3b9fde9f2
Tobias Diekershoff <tobias.diekershoff@gmx.net>
2017-08-09 16:10:09 +0200

    it is hardwrap for consistency

--- library/markdown.php
+++ library/markdown.php
@@ -14,22 +14,22 @@ use \Michelf\MarkdownExtra;
  *
  * This function is using the php-markdown library by Michel Fortin to parse a 
  * string ($text).It returns the rendered HTML code from that text. The optional 
- * $hardbreak parameter is used to switch between inserting hard breaks after
+ * $hardwrap parameter is used to switch between inserting hard breaks after
  * every linefeed, which is required for Diaspora compatibility, or not. The
  * later is used for parsing documentation and README.md files.
  *
  * @param string $text
- * @param boolean $hardbreak
- * @returns string
+ * @param boolean $hardwrap
+ * @return string
  */
 
-function Markdown($text, $hardbreak=true) {
+function Markdown($text, $hardwrap=true) {
 	$a = get_app();
 
 	$stamp1 = microtime(true);
 
 	$MarkdownParser = new MarkdownExtra();
-	if ($hardbreak) {
+	if ($hardwrap) {
 		$MarkdownParser->hard_wrap = true;
 	} else {
 		$MarkdownParser->hard_wrap = false;
