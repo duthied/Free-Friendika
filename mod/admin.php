1893c3df163fa99525ea6aa9c78e2c56947a59c0
Michael <heluecht@pirati.ca>
2017-08-29 16:40:08 +0000

    Pleroma-Servers are now detected and displayed in the statistics

--- mod/admin.php
+++ mod/admin.php
@@ -441,7 +441,7 @@ function admin_page_federation(App $a) {
 	// off one % two of them are needed in the query
 	// Add more platforms if you like, when one returns 0 known nodes it is not
 	// displayed on the stats page.
-	$platforms = array('Friendi%%a', 'Diaspora', '%%red%%', 'Hubzilla', 'BlaBlaNet', 'GNU Social', 'StatusNet', 'Mastodon');
+	$platforms = array('Friendi%%a', 'Diaspora', '%%red%%', 'Hubzilla', 'BlaBlaNet', 'GNU Social', 'StatusNet', 'Mastodon', 'Pleroma');
 	$colors    = array('Friendi%%a' => '#ffc018',     // orange from the logo
 			   'Diaspora'  => '#a1a1a1',     // logo is black and white, makes a gray
 			   '%%red%%'   => '#c50001',     // fire red from the logo
@@ -449,7 +449,8 @@ function admin_page_federation(App $a) {
 			   'BlaBlaNet' => '#3B5998',     // blue from the navbar at blablanet-dot-com
 			   'GNU Social'=> '#a22430',     // dark red from the logo
 			   'StatusNet' => '#789240',     // the green from the logo (red and blue have already others
-			   'Mastodon'  => '#1a9df9');    // blue from the Mastodon logo
+			   'Mastodon'  => '#1a9df9',    // blue from the Mastodon logo
+			   'Pleroma'  => '#E46F0F');    // Orange from the text that is used on Pleroma instances
 	$counts = array();
 	$total = 0;
 
