12b37ac596caa37d82e474cc24e0cf9866540eca
Tobias Diekershoff <tobias.diekershoff@gmx.net>
2017-10-05 10:05:01 +0200

    Friendica Version 3.5.3

--- boot.php
+++ boot.php
@@ -41,7 +41,7 @@ require_once 'include/poller.php';
 
 define ( 'FRIENDICA_PLATFORM',     'Friendica');
 define ( 'FRIENDICA_CODENAME',     'Asparagus');
-define ( 'FRIENDICA_VERSION',      '3.5.3-rc' );
+define ( 'FRIENDICA_VERSION',      '3.5.3' );
 define ( 'DFRN_PROTOCOL_VERSION',  '2.23'    );
 define ( 'DB_UPDATE_VERSION',      1234      );
 
