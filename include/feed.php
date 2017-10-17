e38da9a3a5d9dad56afc877e4f27ad332f9e5f8f
Michael <heluecht@pirati.ca>
2017-10-17 11:39:09 +0000

    And some further refinements

--- include/feed.php
+++ include/feed.php
@@ -353,6 +353,10 @@ function feed_import($xml,$importer,&$contact, &$hub, $simulate = false) {
 				}
 			}
 
+			// Remove a possible link to the item itself
+			$item["body"] = str_replace($item["plink"], '', $item["body"]);
+			$item["body"] = preg_replace('/\[url\=\](\w+.*?)\[\/url\]/i', '', $item["body"]);
+
 			// Replace the content when the title is longer than the body
 			$replace = (strlen($item["title"]) > strlen($item["body"]));
 
@@ -368,8 +372,9 @@ function feed_import($xml,$importer,&$contact, &$hub, $simulate = false) {
 
 			if ($replace) {
 				$item["body"] = $item["title"];
-				$item["title"] = "";
 			}
+			// We always strip the title since it will be added in the page information
+			$item["title"] = "";
 			$item["body"] = $item["body"].add_page_info($item["plink"], false, $preview, ($contact["fetch_further_information"] == 2), $contact["ffi_keyword_blacklist"]);
 			$item["tag"] = add_page_keywords($item["plink"], false, $preview, ($contact["fetch_further_information"] == 2), $contact["ffi_keyword_blacklist"]);
 			$item["object-type"] = ACTIVITY_OBJ_BOOKMARK;
