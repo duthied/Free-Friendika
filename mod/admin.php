7881fd7dc9c5afbd5ec09878729d7b366083fbc2
Tobias Diekershoff <tobias.diekershoff@gmx.net>
2017-11-06 18:20:39 +0100

    std foo

--- mod/admin.php
+++ mod/admin.php
@@ -619,7 +619,7 @@ function admin_page_summary(App $a) {
 	// the local version of Friendica. Check is opt-in, source may be master or devel branch
 	if (Config::get('system', 'check_new_version_url', 'none') != 'none' ) {
 		$gitversion = Config::get('system','git_friendica_version');
-		if (version_compare(FRIENDICA_VERSION, $gitversion)<0) {
+		if (version_compare(FRIENDICA_VERSION, $gitversion) < 0) {
 			$warningtext[] = t('There is a new version of Friendica available for download.');
 			$showwarning = true;
 		}
@@ -1247,7 +1247,7 @@ function admin_page_site(App $a) {
 
 		'$nodeinfo'		=> array('nodeinfo', t("Publish server information"), get_config('system','nodeinfo'), t("If enabled, general server and usage data will be published. The data contains the name and version of the server, number of users with public profiles, number of posts and the activated protocols and connectors. See <a href='http://the-federation.info/'>the-federation.info</a> for details.")),
 
-		'$checknewversionurl' => array('check_new_version_url', t("Check upstream version"), get_config('system', 'check_new_version_url'), t("Enables checking for new Friendica versions at github. If there is a new version, you will be informed in the admin panel overview."), $check_git_version_choices),
+		'$check_new_version_url' => array('check_new_version_url', t("Check upstream version"), get_config('system', 'check_new_version_url'), t("Enables checking for new Friendica versions at github. If there is a new version, you will be informed in the admin panel overview."), $check_git_version_choices),
 		'$suppress_tags'	=> array('suppress_tags', t("Suppress Tags"), get_config('system','suppress_tags'), t("Suppress showing a list of hashtags at the end of the posting.")),
 		'$itemcache'		=> array('itemcache', t("Path to item cache"), get_config('system','itemcache'), t("The item caches buffers generated bbcode and external images.")),
 		'$itemcache_duration' 	=> array('itemcache_duration', t("Cache duration in seconds"), get_config('system','itemcache_duration'), t("How long should the cache files be hold? Default value is 86400 seconds (One day). To disable the item cache, set the value to -1.")),
