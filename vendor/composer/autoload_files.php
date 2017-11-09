3b2cd854837e5df38f75f323aab24e31098812d4
Hypolite Petovan <mrpetovan@gmail.com>
2017-11-09 02:13:02 -0500

    Add defuse/php-encryption 2.0 to Composer dependencies

--- vendor/composer/autoload_files.php
+++ vendor/composer/autoload_files.php
@@ -6,5 +6,6 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
+    '5255c38a0faeba867671b61dfda6d864' => $vendorDir . '/paragonie/random_compat/lib/random.php',
     '2cffec82183ee1cea088009cef9a6fc3' => $vendorDir . '/ezyang/htmlpurifier/library/HTMLPurifier.composer.php',
 );
