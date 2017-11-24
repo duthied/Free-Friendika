365850f26a1cbbcc9e051a1e24fec2b35613b48c
Hypolite Petovan <mrpetovan@gmail.com>
2017-11-23 23:47:52 -0500

    Fix httpdesc to work with new namespace

--- src/Network/HTTPException.php
+++ src/Network/HTTPException.php
@@ -11,12 +11,16 @@ namespace Friendica\Network;
 
 use Exception;
 
-class HTTPException extends Exception {
+class HTTPException extends Exception
+{
 	var $httpcode = 200;
 	var $httpdesc = "";
-	public function __construct($message = "", $code = 0, Exception $previous = null) {
-		if ($this->httpdesc == "") {
-			$this->httpdesc = preg_replace("|([a-z])([A-Z])|",'$1 $2', str_replace("Exception","",get_class($this)));
+
+	public function __construct($message = '', $code = 0, Exception $previous = null)
+	{
+		if ($this->httpdesc == '') {
+			$classname = str_replace('Exception', '', str_replace('Friendica\Network\HTTPException\\', '', get_class($this)));
+			$this->httpdesc = preg_replace("|([a-z])([A-Z])|",'$1 $2', $classname);
 		}
 		parent::__construct($message, $code, $previous);
 	}
