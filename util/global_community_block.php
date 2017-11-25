d9185540a5462207d0256a4c879e3a98809dda23
Tobias Diekershoff <tobias.diekershoff@gmx.net>
2017-11-25 16:45:20 +0100

    some minor stuff

--- util/global_community_block.php
+++ util/global_community_block.php
@@ -19,7 +19,7 @@
  * License: AGPLv3 or later, same as Friendica
  **/
 
-if ($argc!=2 || $argv[1]=="-h" || $argv[1]=="--help" || $argv[1]=="-?") {
+if ($argc != 2 || $argv[1] == "-h" || $argv[1] == "--help" || $argv[1] == "-?") {
 	echo "Usage: ".$argv[0]." [-h|profile_url]\r\n";
 	echo "    -h, -?, --help ... show this help\r\n";
 	echo "    profile_url ...... The URL of the profile you want to silence\r\n";
@@ -33,11 +33,11 @@ if ($argc!=2 || $argv[1]=="-h" || $argv[1]=="--help" || $argv[1]=="-?") {
 use Friendica\Database\DBM;
 use Friendica\Network\Probe;
 
-require_once("boot.php");
+require_once('boot.php');
 require_once('include/dba.php');
-require_once("include/text.php");
+require_once('include/text.php');
 $a = get_app();
-require_once ".htconfig.php";
+require_once('.htconfig.php');
 
 dba::connect($db_host, $db_user, $db_pass, $db_data);
 unset($db_host, $db_user, $db_pass, $db_data);
