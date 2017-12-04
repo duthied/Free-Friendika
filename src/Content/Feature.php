4af9616b178aabbb7383bc150c41f6ec36afbaf2
Adam Magness <adam.magness@gmail.com>
2017-12-04 09:16:15 -0500

    Standards
    Standards clean up.

--- src/Content/Feature.php
+++ src/Content/Feature.php
@@ -15,6 +15,8 @@ class Feature
 	/**
 	 * @brief check if feature is enabled
 	 *
+	 * @param integer $uid     user id
+	 * @param string  $feature feature
 	 * @return boolean
 	 */
 	public static function isEnabled($uid, $feature)
@@ -32,14 +34,14 @@ class Feature
 		}
 
 		$arr = array('uid' => $uid, 'feature' => $feature, 'enabled' => $x);
-		call_hooks('isEnabled',$arr);
+		call_hooks('isEnabled', $arr);
 		return($arr['enabled']);
 	}
 
 	/**
 	 * @brief check if feature is enabled or disabled by default
 	 *
-	 * @param string $feature
+	 * @param string $feature feature
 	 * @return boolean
 	 */
 	private static function getDefault($feature)
@@ -47,8 +49,9 @@ class Feature
 		$f = self::get();
 		foreach ($f as $cat) {
 			foreach ($cat as $feat) {
-				if (is_array($feat) && $feat[0] === $feature)
+				if (is_array($feat) && $feat[0] === $feature) {
 					return $feat[3];
+				}
 			}
 		}
 		return false;
@@ -73,53 +76,53 @@ class Feature
 			'general' => array(
 				t('General Features'),
 				//array('expire',         t('Content Expiration'),		t('Remove old posts/comments after a period of time')),
-				array('multi_profiles', t('Multiple Profiles'),			t('Ability to create multiple profiles'), false, Config::get('feature_lock','multi_profiles', false)),
-				array('photo_location', t('Photo Location'),			t('Photo metadata is normally stripped. This extracts the location (if present) prior to stripping metadata and links it to a map.'), false, Config::get('feature_lock','photo_location', false)),
-				array('export_calendar', t('Export Public Calendar'),		t('Ability for visitors to download the public calendar'), false, Config::get('feature_lock','export_calendar', false)),
+				array('multi_profiles', t('Multiple Profiles'),			t('Ability to create multiple profiles'), false, Config::get('feature_lock', 'multi_profiles', false)),
+				array('photo_location', t('Photo Location'),			t('Photo metadata is normally stripped. This extracts the location (if present) prior to stripping metadata and links it to a map.'), false, Config::get('feature_lock', 'photo_location', false)),
+				array('export_calendar', t('Export Public Calendar'),		t('Ability for visitors to download the public calendar'), false, Config::get('feature_lock', 'export_calendar', false)),
 			),
 
 			// Post composition
 			'composition' => array(
 				t('Post Composition Features'),
-				array('preview',	t('Post Preview'),			t('Allow previewing posts and comments before publishing them'), false, Config::get('feature_lock','preview', false)),
-				array('aclautomention',	t('Auto-mention Forums'),		t('Add/remove mention when a forum page is selected/deselected in ACL window.'), false, Config::get('feature_lock','aclautomention', false)),
+				array('preview',	t('Post Preview'),			t('Allow previewing posts and comments before publishing them'), false, Config::get('feature_lock', 'preview', false)),
+				array('aclautomention',	t('Auto-mention Forums'),		t('Add/remove mention when a forum page is selected/deselected in ACL window.'), false, Config::get('feature_lock', 'aclautomention', false)),
 			),
 
 			// Network sidebar widgets
 			'widgets' => array(
 				t('Network Sidebar Widgets'),
-				array('archives',	t('Search by Date'),			t('Ability to select posts by date ranges'), false, Config::get('feature_lock','archives', false)),
-				array('forumlist_widget', t('List Forums'),			t('Enable widget to display the forums your are connected with'), true, Config::get('feature_lock','forumlist_widget', false)),
-				array('groups',		t('Group Filter'),			t('Enable widget to display Network posts only from selected group'), false, Config::get('feature_lock','groups', false)),
-				array('networks',	t('Network Filter'),			t('Enable widget to display Network posts only from selected network'), false, Config::get('feature_lock','networks', false)),
-				array('savedsearch',	t('Saved Searches'),			t('Save search terms for re-use'), false, Config::get('feature_lock','savedsearch', false)),
+				array('archives',	t('Search by Date'),			t('Ability to select posts by date ranges'), false, Config::get('feature_lock', 'archives', false)),
+				array('forumlist_widget', t('List Forums'),			t('Enable widget to display the forums your are connected with'), true, Config::get('feature_lock', 'forumlist_widget', false)),
+				array('groups',		t('Group Filter'),			t('Enable widget to display Network posts only from selected group'), false, Config::get('feature_lock', 'groups', false)),
+				array('networks',	t('Network Filter'),			t('Enable widget to display Network posts only from selected network'), false, Config::get('feature_lock', 'networks', false)),
+				array('savedsearch',	t('Saved Searches'),			t('Save search terms for re-use'), false, Config::get('feature_lock', 'savedsearch', false)),
 			),
 
 			// Network tabs
 			'net_tabs' => array(
 				t('Network Tabs'),
-				array('personal_tab',	t('Network Personal Tab'),		t('Enable tab to display only Network posts that you\'ve interacted on'), false, Config::get('feature_lock','personal_tab', false)),
-				array('new_tab',	t('Network New Tab'),			t('Enable tab to display only new Network posts (from the last 12 hours)'), false, Config::get('feature_lock','new_tab', false)),
-				array('link_tab',	t('Network Shared Links Tab'),		t('Enable tab to display only Network posts with links in them'), false, Config::get('feature_lock','link_tab', false)),
+				array('personal_tab',	t('Network Personal Tab'),		t('Enable tab to display only Network posts that you\'ve interacted on'), false, Config::get('feature_lock', 'personal_tab', false)),
+				array('new_tab',	t('Network New Tab'),			t('Enable tab to display only new Network posts (from the last 12 hours)'), false, Config::get('feature_lock', 'new_tab', false)),
+				array('link_tab',	t('Network Shared Links Tab'),		t('Enable tab to display only Network posts with links in them'), false, Config::get('feature_lock', 'link_tab', false)),
 			),
 
 			// Item tools
 			'tools' => array(
 				t('Post/Comment Tools'),
-				array('multi_delete',	t('Multiple Deletion'),			t('Select and delete multiple posts/comments at once'), false, Config::get('feature_lock','multi_delete', false)),
-				array('edit_posts',	t('Edit Sent Posts'),			t('Edit and correct posts and comments after sending'), false, Config::get('feature_lock','edit_posts', false)),
-				array('commtag',	t('Tagging'),				t('Ability to tag existing posts'), false, Config::get('feature_lock','commtag', false)),
-				array('categories',	t('Post Categories'),			t('Add categories to your posts'), false, Config::get('feature_lock','categories', false)),
-				array('filing',		t('Saved Folders'),			t('Ability to file posts under folders'), false, Config::get('feature_lock','filing', false)),
-				array('dislike',	t('Dislike Posts'),			t('Ability to dislike posts/comments'), false, Config::get('feature_lock','dislike', false)),
-				array('star_posts',	t('Star Posts'),			t('Ability to mark special posts with a star indicator'), false, Config::get('feature_lock','star_posts', false)),
-				array('ignore_posts',	t('Mute Post Notifications'),		t('Ability to mute notifications for a thread'), false, Config::get('feature_lock','ignore_posts', false)),
+				array('multi_delete',	t('Multiple Deletion'),			t('Select and delete multiple posts/comments at once'), false, Config::get('feature_lock', 'multi_delete', false)),
+				array('edit_posts',	t('Edit Sent Posts'),			t('Edit and correct posts and comments after sending'), false, Config::get('feature_lock', 'edit_posts', false)),
+				array('commtag',	t('Tagging'),				t('Ability to tag existing posts'), false, Config::get('feature_lock', 'commtag', false)),
+				array('categories',	t('Post Categories'),			t('Add categories to your posts'), false, Config::get('feature_lock', 'categories', false)),
+				array('filing',		t('Saved Folders'),			t('Ability to file posts under folders'), false, Config::get('feature_lock', 'filing', false)),
+				array('dislike',	t('Dislike Posts'),			t('Ability to dislike posts/comments'), false, Config::get('feature_lock', 'dislike', false)),
+				array('star_posts',	t('Star Posts'),			t('Ability to mark special posts with a star indicator'), false, Config::get('feature_lock', 'star_posts', false)),
+				array('ignore_posts',	t('Mute Post Notifications'),		t('Ability to mute notifications for a thread'), false, Config::get('feature_lock', 'ignore_posts', false)),
 			),
 
 			// Advanced Profile Settings
 			'advanced_profile' => array(
 				t('Advanced Profile Settings'),
-				array('forumlist_profile', t('List Forums'),			t('Show visitors public community forums at the Advanced Profile Page'), false, Config::get('feature_lock','forumlist_profile', false)),
+				array('forumlist_profile', t('List Forums'),			t('Show visitors public community forums at the Advanced Profile Page'), false, Config::get('feature_lock', 'forumlist_profile', false)),
 				array('tagadelic',	t('Tag Cloud'),				t('Provide a personal tag cloud on your profile page'), false, Config::get('feature_lock', 'tagadelic', false)),
 			),
 		);
