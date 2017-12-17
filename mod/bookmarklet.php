2cfcf433a33a7781d7ae79cf325ca7f0b72edeb3
Hypolite Petovan <mrpetovan@gmail.com>
2017-12-16 20:13:10 -0500

    Fix formatting in mod
    - mod/api
    - mod/bookmarklet
    - mod/dfrn_request

--- mod/bookmarklet.php
+++ mod/bookmarklet.php
@@ -6,19 +6,21 @@ use Friendica\Core\System;
 require_once('include/conversation.php');
 require_once('include/items.php');
 
-function bookmarklet_init(App $a) {
+function bookmarklet_init(App $a)
+{
 	$_GET["mode"] = "minimal";
 }
 
-function bookmarklet_content(App $a) {
+function bookmarklet_content(App $a)
+{
 	if (!local_user()) {
-		$o = '<h2>'.t('Login').'</h2>';
 		$o .= login(($a->config['register_policy'] == REGISTER_CLOSED) ? false : true);
+		$o = '<h2>' . t('Login') . '</h2>';
 		return $o;
 	}
 
 	$referer = normalise_link($_SERVER["HTTP_REFERER"]);
-	$page = normalise_link(System::baseUrl()."/bookmarklet");
+	$page = normalise_link(System::baseUrl() . "/bookmarklet");
 
 	if (!strstr($referer, $page)) {
 		$content = add_page_info($_REQUEST["url"]);
@@ -30,7 +32,7 @@ function bookmarklet_content(App $a) {
 			'nickname' => $a->user['nickname'],
 			'lockstate' => ((is_array($a->user) && ((strlen($a->user['allow_cid'])) || (strlen($a->user['allow_gid'])) || (strlen($a->user['deny_cid'])) || (strlen($a->user['deny_gid'])))) ? 'lock' : 'unlock'),
 			'default_perms' => get_acl_permissions($a->user),
-			'acl' => populate_acl($a->user,true),
+			'acl' => populate_acl($a->user, true),
 			'bang' => '',
 			'visitor' => 'block',
 			'profile_uid' => local_user(),
@@ -38,10 +40,10 @@ function bookmarklet_content(App $a) {
 			'title' => trim($_REQUEST["title"], "*"),
 			'content' => $content
 		);
-		$o = status_editor($a,$x, 0, false);
+		$o = status_editor($a, $x, 0, false);
 		$o .= "<script>window.resizeTo(800,550);</script>";
 	} else {
-		$o = '<h2>'.t('The post was created').'</h2>';
+		$o = '<h2>' . t('The post was created') . '</h2>';
 		$o .= "<script>window.close()</script>";
 	}
 
