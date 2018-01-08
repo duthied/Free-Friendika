b007823bcaa81649e471acaa8c48b4c93cd89810
Michael <heluecht@pirati.ca>
2018-01-08 06:59:49 +0000

    Only do it when public commenting is enabled

--- mod/network.php
+++ mod/network.php
@@ -807,7 +807,7 @@ function networkThreadedView(App $a, $update = 0) {
 	}
 
 	// Only show it when unfiltered (no groups, no networks, ...)
-	if ((count($r) > 0) && (strlen($sql_extra . $sql_extra2 . $sql_extra3 . $sql_extra4 . $sql_nets) == 0)) {
+	if (Config::get('system', 'comment_public') && (count($r) > 0) && (strlen($sql_extra . $sql_extra2 . $sql_extra3 . $sql_extra4 . $sql_nets) == 0)) {
 		$top_limit = current($r)['order_date'];
 		$bottom_limit = end($r)['order_date'];
 
