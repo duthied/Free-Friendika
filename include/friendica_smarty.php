e412cd7c51998afad2c8c70385dbcf8a84ce4cfc
Hypolite Petovan <mrpetovan@gmail.com>
2018-01-14 13:36:48 -0500

    Fix formatting in include/friendica_smarty

--- include/friendica_smarty.php
+++ include/friendica_smarty.php
@@ -4,12 +4,14 @@ use Friendica\Render\ITemplateEngine;
 
 require_once "include/plugin.php";
 
-define('SMARTY3_TEMPLATE_FOLDER','templates');
+define('SMARTY3_TEMPLATE_FOLDER', 'templates');
 
-class FriendicaSmarty extends Smarty {
+class FriendicaSmarty extends Smarty
+{
 	public $filename;
 
-	function __construct() {
+	function __construct()
+	{
 		parent::__construct();
 
 		$a = get_app();
@@ -17,10 +19,10 @@ class FriendicaSmarty extends Smarty {
 
 		// setTemplateDir can be set to an array, which Smarty will parse in order.
 		// The order is thus very important here
-		$template_dirs = array('theme' => "view/theme/$theme/".SMARTY3_TEMPLATE_FOLDER."/");
-		if ( x($a->theme_info,"extends") )
-			$template_dirs = $template_dirs + array('extends' => "view/theme/".$a->theme_info["extends"]."/".SMARTY3_TEMPLATE_FOLDER."/");
-		$template_dirs = $template_dirs + array('base' => "view/".SMARTY3_TEMPLATE_FOLDER."/");
+		$template_dirs = array('theme' => "view/theme/$theme/" . SMARTY3_TEMPLATE_FOLDER . "/");
+		if (x($a->theme_info, "extends"))
+			$template_dirs = $template_dirs + array('extends' => "view/theme/" . $a->theme_info["extends"] . "/" . SMARTY3_TEMPLATE_FOLDER . "/");
+		$template_dirs = $template_dirs + array('base' => "view/" . SMARTY3_TEMPLATE_FOLDER . "/");
 		$this->setTemplateDir($template_dirs);
 
 		$this->setCompileDir('view/smarty3/compiled/');
@@ -34,27 +36,31 @@ class FriendicaSmarty extends Smarty {
 		$this->error_reporting = E_ALL & ~E_NOTICE;
 	}
 
-	function parsed($template = '') {
+	function parsed($template = '')
+	{
 		if ($template) {
 			return $this->fetch('string:' . $template);
 		}
 		return $this->fetch('file:' . $this->filename);
 	}
 
-
 }
 
-class FriendicaSmartyEngine implements ITemplateEngine {
-	static $name ="smarty3";
+class FriendicaSmartyEngine implements ITemplateEngine
+{
+	static $name = "smarty3";
 
-    public function __construct(){
-		if (!is_writable('view/smarty3/')){
-			echo "<b>ERROR:</b> folder <tt>view/smarty3/</tt> must be writable by webserver."; killme();
+	public function __construct()
+	{
+		if (!is_writable('view/smarty3/')) {
+			echo "<b>ERROR:</b> folder <tt>view/smarty3/</tt> must be writable by webserver.";
+			killme();
 		}
 	}
 
 	// ITemplateEngine interface
-	public function replaceMacros($s, $r) {
+	public function replaceMacros($s, $r)
+	{
 		$template = '';
 		if (gettype($s) === 'string') {
 			$template = $s;
@@ -65,13 +71,13 @@ class FriendicaSmartyEngine implements ITemplateEngine {
 
 		// "middleware": inject variables into templates
 		$arr = array(
-			"template"=> basename($s->filename),
+			"template" => basename($s->filename),
 			"vars" => $r
 		);
 		call_hooks("template_vars", $arr);
 		$r = $arr['vars'];
 
-		foreach ($r as $key=>$value) {
+		foreach ($r as $key => $value) {
 			if ($key[0] === '$') {
 				$key = substr($key, 1);
 			}
@@ -80,9 +86,10 @@ class FriendicaSmartyEngine implements ITemplateEngine {
 		return $s->parsed($template);
 	}
 
-	public function getTemplateFile($file, $root=''){
+	public function getTemplateFile($file, $root = '')
+	{
 		$a = get_app();
-		$template_file = get_template_file($a, SMARTY3_TEMPLATE_FOLDER.'/'.$file, $root);
+		$template_file = get_template_file($a, SMARTY3_TEMPLATE_FOLDER . '/' . $file, $root);
 		$template = new FriendicaSmarty();
 		$template->filename = $template_file;
 		return $template;
