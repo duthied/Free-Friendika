98edd826acde78593644c562fcad934ccf12bdd0
Michael <heluecht@pirati.ca>
2018-01-14 15:14:16 +0000

    Increased database version

--- boot.php
+++ boot.php
@@ -43,7 +43,7 @@ define('FRIENDICA_PLATFORM',     'Friendica');
 define('FRIENDICA_CODENAME',     'Asparagus');
 define('FRIENDICA_VERSION',      '3.6-dev');
 define('DFRN_PROTOCOL_VERSION',  '2.23');
-define('DB_UPDATE_VERSION',      1241);
+define('DB_UPDATE_VERSION',      1242);
 define('NEW_UPDATE_ROUTINE_VERSION', 1170);
 
 /**
