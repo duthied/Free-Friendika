3e56a631620ccac6268e756f56b855314243347f
Hypolite Petovan <mrpetovan@gmail.com>
2018-01-14 10:06:12 -0500

    Remove library/php-markdown

--- library/markdown.php
+++ library/markdown.php
@@ -6,14 +6,13 @@
  * @brief Parser for Markdown files
  */
 
-require_once "library/php-markdown/Michelf/MarkdownExtra.inc.php";
 use \Michelf\MarkdownExtra;
 
 /**
  * @brief This function parses a text using php-markdown library to render Markdown syntax to HTML
  *
- * This function is using the php-markdown library by Michel Fortin to parse a 
- * string ($text).It returns the rendered HTML code from that text. The optional 
+ * This function is using the php-markdown library by Michel Fortin to parse a
+ * string ($text).It returns the rendered HTML code from that text. The optional
  * $hardwrap parameter is used to switch between inserting hard breaks after
  * every linefeed, which is required for Diaspora compatibility, or not. The
  * later is used for parsing documentation and README.md files.
