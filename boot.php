a01a8a06f28c41b79d8818f8cd26fed0e4c55fa7
Adam Magness <adam.magness@gmail.com>
2018-01-14 21:26:07 -0500

    Remove requires
    remove requires_once statment

--- boot.php
+++ boot.php
@@ -37,7 +37,6 @@ require_once 'include/text.php';
 require_once 'include/datetime.php';
 require_once 'include/pgettext.php';
 require_once 'include/nav.php';
-require_once 'include/identity.php';
 
 define('FRIENDICA_PLATFORM',     'Friendica');
 define('FRIENDICA_CODENAME',     'Asparagus');
