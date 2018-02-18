d0fa80b195a43e2447e1a9e9242fab7ee9617f4b
Michael <heluecht@pirati.ca>
2018-02-18 17:29:14 +0000

    Issue 4228: Don't post an item if it belongs to a picture upload without an post

--- src/Protocol/OStatus.php
+++ src/Protocol/OStatus.php
@@ -2128,6 +2128,7 @@ class OStatus
 				AND `item`.`created` > '%s'
 				AND NOT `item`.`deleted`
 				AND NOT `item`.`private`
+				AND `item`.`visible`
 				AND `thread`.`network` IN ('%s', '%s')
 				$sql_extra
 				ORDER BY `item`.`created` DESC LIMIT %d",
