4b361747a093121e1fb26b03911e2dcc9380487d
Hypolite Petovan <mrpetovan@gmail.com>
2018-02-28 23:48:28 -0500

    Update SQL scripts for cache table

--- database.sql
+++ database.sql
@@ -1,6 +1,6 @@
 -- ------------------------------------------
 -- Friendica 3.6-dev (Asparagus)
--- DB_UPDATE_VERSION 1255
+-- DB_UPDATE_VERSION 1256
 -- ------------------------------------------
 
 
@@ -55,13 +55,13 @@ CREATE TABLE IF NOT EXISTS `auth_codes` (
 -- TABLE cache
 --
 CREATE TABLE IF NOT EXISTS `cache` (
-	`k` varbinary(255) NOT NULL COMMENT '',
-	`v` mediumtext COMMENT '',
-	`expire_mode` tinyint unsigned NOT NULL DEFAULT 0 COMMENT '',
-	`updated` datetime NOT NULL DEFAULT '0001-01-01 00:00:00' COMMENT '',
-	 PRIMARY KEY(`k`),
-	 INDEX `expire_mode_updated` (`expire_mode`,`updated`)
-) DEFAULT COLLATE utf8mb4_general_ci;
+	`k` varbinary(255) NOT NULL COMMENT 'cache key',
+	`v` mediumtext COMMENT 'cached serialized value',
+	`expires` datetime NOT NULL DEFAULT '0001-01-01 00:00:00' COMMENT 'datetime of cache expiration',
+	`updated` datetime NOT NULL DEFAULT '0001-01-01 00:00:00' COMMENT 'datetime of cache insertion',
+	PRIMARY KEY (`k`),
+	KEY `k_expires` (`k`,`expires`) USING BTREE
+) DEFAULT COLLATE utf8mb4_general_ci COMMENT='Stores temporary data';
 
 --
 -- TABLE challenge
