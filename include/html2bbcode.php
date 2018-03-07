f633acd8d16053f8135a79b6abf1527a94732fa4
Hypolite Petovan <mrpetovan@gmail.com>
2018-03-07 16:34:48 -0500

    Remove include/html2*.php files

--- include/html2bbcode.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-function html2bbcode($message, $basepath = '')
-{
-	return Friendica\Content\Text\HTML::toBBCode($message, $basepath);
-}
--- include/html2plain.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-function html2plain($html, $wraplength = 75, $compact = false)
-{
-	return Friendica\Content\Text\HTML::toPlaintext($html, $wraplength, $compact);
-}
