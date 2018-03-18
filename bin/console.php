dcf2ed26bb19f908f5b9268f938e70d9396d6ccf
Hypolite Petovan <mrpetovan@gmail.com>
2018-03-18 05:12:39 -0400

    Add console executables

---
+++ bin/console.php
@@ -0,0 +1,9 @@
+#!/bin/env php
+<?php
+
+include_once dirname(__DIR__) . '/boot.php';
+
+$a = new Friendica\App(dirname(__DIR__));
+\Friendica\BaseObject::setApp($a);
+
+(new Friendica\Core\Console())->execute();
