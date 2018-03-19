542ff9b0825bc2b21137f23026d61c2c9644ef13
Hypolite Petovan <mrpetovan@gmail.com>
2018-03-18 23:17:31 -0400

    Move scripts/auth_ejabberd.php to bin/

--- bin/auth_ejabberd.php
+++ bin/auth_ejabberd.php
@@ -13,14 +13,14 @@
  * Installation:
  *
  * 	- Change it's owner to whichever user is running the server, ie. ejabberd
- * 	  $ chown ejabberd:ejabberd /path/to/friendica/scripts/auth_ejabberd.php
+ * 	  $ chown ejabberd:ejabberd /path/to/friendica/bin/auth_ejabberd.php
  *
  * 	- Change the access mode so it is readable only to the user ejabberd and has exec
- * 	  $ chmod 700 /path/to/friendica/scripts/auth_ejabberd.php
+ * 	  $ chmod 700 /path/to/friendica/bin/auth_ejabberd.php
  *
  * 	- Edit your ejabberd.cfg file, comment out your auth_method and add:
  * 	  {auth_method, external}.
- * 	  {extauth_program, "/path/to/friendica/script/auth_ejabberd.php"}.
+ * 	  {extauth_program, "/path/to/friendica/bin/auth_ejabberd.php"}.
  *
  * 	- Restart your ejabberd service, you should be able to login with your friendica auth info
  *
