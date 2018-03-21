73efdb99e3ca9395f15e89db49dc4b5768926f26
Michael <heluecht@pirati.ca>
2018-03-21 05:20:55 +0000

    Fix for incorrect datetime values

--- src/Protocol/PortableContact.php
+++ src/Protocol/PortableContact.php
@@ -945,6 +945,15 @@ class PortableContact
 			$register_policy = $gserver["register_policy"];
 			$registered_users = $gserver["registered-users"];
 
+			// See discussion under https://forum.friendi.ca/display/0b6b25a8135aabc37a5a0f5684081633
+			// It can happen that a zero date is in the database, but storing it again is forbidden.
+			if ($last_contact < NULL_DATE) {
+				$last_contact = NULL_DATE;
+			}
+			if ($last_failure < NULL_DATE) {
+				$last_failure = NULL_DATE;
+			}
+
 			if (!$force && !self::updateNeeded($gserver["created"], "", $last_failure, $last_contact)) {
 				logger("Use cached data for server ".$server_url, LOGGER_DEBUG);
 				return ($last_contact >= $last_failure);
