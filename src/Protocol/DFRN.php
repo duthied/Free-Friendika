3bb5a416f8d6bfe7e6b1de3debe2828d7e50d3cc
Michael <heluecht@pirati.ca>
2018-05-02 21:12:48 +0000

    Some corrections

--- src/Protocol/DFRN.php
+++ src/Protocol/DFRN.php
@@ -2094,6 +2094,7 @@ class DFRN
 			'confirm' => $relocate["confirm"], 'notify' => $relocate["notify"],
 			'poll' => $relocate["poll"], 'site-pubkey' => $relocate["sitepubkey"]];
 		$condition = ["(`id` = ?) OR (`nurl` = ?)", $importer["id"], normalise_link($old["url"])];
+		dba::update('contact', $fields, $condition);
 
 		// @TODO No dba:update here?
 		dba::update('contact', $fields, $condition);
