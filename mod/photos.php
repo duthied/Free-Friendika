5a7bcd47258994afa2ba2720d1e80c6b3f87a118
Roland Häder <roland@mxchange.org>
2018-05-14 22:34:11 +0200

    Added empty line after if() block (convention)
    Signed-off-by: Roland Häder <roland@mxchange.org>

--- mod/photos.php
+++ mod/photos.php
@@ -1285,6 +1285,7 @@ function photos_content(App $a)
 		if (count($ph) == 1) {
 			$hires = $lores = $ph[0];
 		}
+
 		if (count($ph) > 1) {
 			if ($ph[1]['scale'] == 2) {
 				// original is 640 or less, we can display it directly
