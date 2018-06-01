6d20785005c22784f1a09120dc94c3a5372d6532
Hypolite Petovan <mrpetovan@gmail.com>
2018-06-01 07:25:48 -0400

    Fix version number in database.sql

--- database.sql
+++ database.sql
@@ -1,5 +1,5 @@
 -- ------------------------------------------
--- Friendica 2018.05-rc (The Tazmans Flax-lily)
+-- Friendica 2018.05 (The Tazmans Flax-lily)
 -- DB_UPDATE_VERSION 1266
 -- ------------------------------------------
 
