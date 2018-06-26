8875cc7419dae5d56c4d09e7d8321e5bfb1057c7
Zane C. Bowers-Hadley <vvelox@vvelox.net>
2018-06-26 05:03:45 -0500

    remove duplicate check

--- src/Util/Network.php
+++ src/Util/Network.php
@@ -495,7 +495,7 @@ class Network
 		if (($h) && (dns_get_record($h, DNS_A + DNS_MX) || filter_var($h, FILTER_VALIDATE_IP) )) {
 			return true;
 		}
-		if (($h) && (dns_get_record($h, DNS_CNAME + DNS_MX) || filter_var($h, FILTER_VALIDATE_IP) )) {
+		if (($h) && dns_get_record($h, DNS_CNAME + DNS_MX)) {
 			return true;
 		}
 		return false;
