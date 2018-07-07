3433c22116bb2e32dc3ce0072cd5bc8b04a92302
Hypolite Petovan <mrpetovan@gmail.com>
2018-07-06 21:17:04 -0400

    Remove .htconfig.php reference from DatabaseTest

--- tests/DatabaseTest.php
+++ tests/DatabaseTest.php
@@ -21,54 +21,6 @@ abstract class DatabaseTest extends TestCase
 	use TestCaseTrait;
 
 	/**
-	 * Renames an eventually existing .htconfig.php to .htconfig.php.tmp
-	 * Creates a new .htconfig.php for bin/worker.php execution
-	 */
-	public static function setUpBeforeClass()
-	{
-		parent::setUpBeforeClass();
-
-		$base_config_file_name = 'htconfig.php';
-		$config_file_name = '.htconfig.php';
-
-		$base_config_file_path = stream_resolve_include_path($base_config_file_name);
-		$config_file_path = dirname($base_config_file_path) . DIRECTORY_SEPARATOR . $config_file_name;
-		$config_file_path_tmp = $config_file_path . '.tmp';
-
-		if (file_exists($config_file_path)) {
-			rename($config_file_path, $config_file_path_tmp);
-		}
-
-		$config_string = file_get_contents($base_config_file_path);
-
-		$config_string = str_replace('die(', '// die(', $config_string);
-
-		file_put_contents($config_file_path, $config_string);
-	}
-
-	/**
-	 * Delete the created .htconfig.php
-	 * Renames an eventually existing .htconfig.php.tmp to .htconfig.php
-	 */
-	public static function tearDownAfterClass()
-	{
-		$base_config_file_name = 'htconfig.php';
-		$config_file_name = '.htconfig.php';
-
-		$base_config_file_path = stream_resolve_include_path($base_config_file_name);
-		$config_file_path = dirname($base_config_file_path) . DIRECTORY_SEPARATOR . $config_file_name;
-		$config_file_path_tmp = $config_file_path . '.tmp';
-
-		if (file_exists($config_file_path)) {
-			unlink($config_file_path);
-		}
-
-		if (file_exists($config_file_path_tmp)) {
-			rename($config_file_path_tmp, $config_file_path);
-		}
-	}
-
-	/**
 	 * Get database connection.
 	 *
 	 * This function is executed before each test in order to get a database connection that can be used by tests.
