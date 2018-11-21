9342888bd594d882edfdbcecefb3f735ab284c61
fabrixxm <fabrix.xm@gmail.com>
2018-11-21 16:21:57 +0100

    Remove default from backend columns
    https://dev.mysql.com/doc/refman/8.0/en/blob.html
    BLOB and TEXT columns cannot have DEFAULT values.

--- config/dbstructure.config.php
+++ config/dbstructure.config.php
@@ -955,8 +955,8 @@ return [
 			"allow_gid" => ["type" => "mediumtext", "comment" => "Access Control - list of allowed groups"],
 			"deny_cid" => ["type" => "mediumtext", "comment" => "Access Control - list of denied contact.id"],
 			"deny_gid" => ["type" => "mediumtext", "comment" => "Access Control - list of denied groups"],
-			"backend-class" => ["type" => "tinytext", "default" => "", "comment" => "Storage backend class"],
-			"backend-ref" => ["type" => "text", "default" => "", "comment" => "Storage backend data reference"]
+			"backend-class" => ["type" => "tinytext", "comment" => "Storage backend class"],
+			"backend-ref" => ["type" => "text", "comment" => "Storage backend data reference"]
 		],
 		"indexes" => [
 			"PRIMARY" => ["id"],
