6fd294aac5876e178e45cb7c857a6c68511426d1
Hypolite Petovan <hypolite@mrpetovan.com>
2018-11-22 22:21:57 -0500

    Protect OAuthException declaration to avoid conflict with php-pecl-oauth

--- library/OAuth1.php
+++ library/OAuth1.php
@@ -3,8 +3,10 @@
 
 /* Generic exception class
  */
-class OAuthException extends Exception {
-  // pass
+if (!class_exists('OAuthException', false)) {
+  class OAuthException extends Exception {
+    // pass
+  }
 }
 
 class OAuthConsumer {
