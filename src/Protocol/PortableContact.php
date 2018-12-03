d76003797a47b38a729d3f5df83d16545dd941ab
Michael <heluecht@pirati.ca>
2018-12-03 16:32:03 +0000

    Fix for notice

--- src/Protocol/PortableContact.php
+++ src/Protocol/PortableContact.php
@@ -734,7 +734,7 @@ class PortableContact
 			}
 		}
 
-		if (is_array($nodeinfo['metadata']) && isset($nodeinfo['metadata']['nodeName'])) {
+		if (!empty($nodeinfo['metadata']) && isset($nodeinfo['metadata']['nodeName'])) {
 			$server['site_name'] = $nodeinfo['metadata']['nodeName'];
 		}
 
@@ -817,7 +817,7 @@ class PortableContact
 			}
 		}
 
-		if (is_array($nodeinfo['metadata']) && isset($nodeinfo['metadata']['nodeName'])) {
+		if (!empty($nodeinfo['metadata']) && isset($nodeinfo['metadata']['nodeName'])) {
 			$server['site_name'] = $nodeinfo['metadata']['nodeName'];
 		}
 
