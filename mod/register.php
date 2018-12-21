1c58363696f3837d5deb882d6090bf1df2da4b0f
Hypolite Petovan <hypolite@mrpetovan.com>
2018-12-20 23:59:15 -0500

    Remove unused/empty template variables in register

--- mod/register.php
+++ mod/register.php
@@ -213,19 +213,14 @@ function register_content(App $a)
 	$noid = Config::get('system', 'no_openid');
 
 	if ($noid) {
-		$oidhtml  = '';
 		$fillwith = '';
 		$fillext  = '';
 		$oidlabel = '';
 	} else {
-		$oidhtml  = '<label for="register-openid" id="label-register-openid" >$oidlabel</label><input type="text" maxlength="60" size="32" name="openid_url" class="openid" id="register-openid" value="$openid" >';
 		$fillwith = L10n::t("You may \x28optionally\x29 fill in this form via OpenID by supplying your OpenID and clicking 'Register'.");
 		$fillext  = L10n::t('If you are not familiar with OpenID, please leave that field blank and fill in the rest of the items.');
 		$oidlabel = L10n::t("Your OpenID \x28optional\x29: ");
 	}
-
-	// I set this and got even more fake names than before...
-	$realpeople = ''; // L10n::t('Members of this network prefer to communicate with real people who use their real names.');
 
 	if (Config::get('system', 'publish_all')) {
 		$profile_publish = '<input type="hidden" name="profile_publish_reg" value="1" />';
@@ -244,8 +239,6 @@ function register_content(App $a)
 	$r = q("SELECT COUNT(*) AS `contacts` FROM `contact`");
 	$passwords = !$r[0]["contacts"];
 
-	$license = '';
-
 	$tpl = Renderer::getMarkupTemplate("register.tpl");
 
 	$arr = ['template' => $tpl];
@@ -257,14 +250,12 @@ function register_content(App $a)
 	$tos = new Tos();
 
 	$o = Renderer::replaceMacros($tpl, [
-		'$oidhtml' => $oidhtml,
 		'$invitations' => Config::get('system', 'invitation_only'),
 		'$permonly'    => intval(Config::get('config', 'register_policy')) === REGISTER_APPROVE,
 		'$permonlybox' => ['permonlybox', L10n::t('Note for the admin'), '', L10n::t('Leave a message for the admin, why you want to join this node')],
 		'$invite_desc' => L10n::t('Membership on this site is by invitation only.'),
 		'$invite_label' => L10n::t('Your invitation code: '),
 		'$invite_id'  => $invite_id,
-		'$realpeople' => $realpeople,
 		'$regtitle'  => L10n::t('Registration'),
 		'$registertext' => BBCode::convert(Config::get('config', 'register_text', '')),
 		'$fillwith'  => $fillwith,
@@ -284,7 +275,6 @@ function register_content(App $a)
 		'$username'  => $username,
 		'$email'     => $email,
 		'$nickname'  => $nickname,
-		'$license'   => $license,
 		'$sitename'  => $a->getHostName(),
 		'$importh'   => L10n::t('Import'),
 		'$importt'   => L10n::t('Import your profile to this friendica instance'),
