0c4f8277e6f0f419b23cc4d939ce5e50904d4462
Hypolite Petovan <hypolite@mrpetovan.com>
2019-01-07 12:26:46 -0500

    Update namespace in mod/view

--- mod/view.php
+++ mod/view.php
@@ -1,10 +1,14 @@
 <?php
+
+use Friendica\App;
+
 /**
  * load view/theme/$current_theme/style.php with friendica context
  *
  * @param App $a
  */
-function view_init($a){
+function view_init(App $a)
+{
 	header("Content-Type: text/css");
 		
 	if ($a->argc == 4){
