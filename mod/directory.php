182bffcc6b8481e002a33b65653557012fd6ba67
Hypolite Petovan <hypolite@mrpetovan.com>
2019-01-09 16:24:21 -0500

    Fix wrong git-fu in mod/directory

--- mod/directory.php
+++ mod/directory.php
@@ -114,8 +114,6 @@ function directory_content(App $a)
 		$entries = [];
 
 		while ($rr = DBA::fetch($r)) {
-
-		while ($rr = DBA::fetch($r)) {
 			$itemurl = (($rr['addr'] != "") ? $rr['addr'] : $rr['profile_url']);
 
 			$profile_link = $rr['profile_url'];
