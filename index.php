3b5f55d3c6555a26365a9f21bd2e154eb0aafbcf
Michael <heluecht@pirati.ca>
2019-01-18 19:10:05 +0000

    Check if the vendor path is missing and complain about it

--- index.php
+++ index.php
@@ -4,6 +4,10 @@
  * Friendica
  */
 
+if (!file_exists(__DIR__ . '/vendor/autoload.php')) {
+	die('Vendor path not found. Please execute "bin/composer.phar --no-dev install" on the command line in the web root.');
+}
+
 require __DIR__ . '/vendor/autoload.php';
 
 // We assume that the index.php is called by a frontend process
