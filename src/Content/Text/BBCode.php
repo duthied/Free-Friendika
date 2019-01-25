41663c7592abe55a958632c3a48265e0a8544d2b
Peter Liebetrau <peter.liebetrau@gmail.com>
2019-01-25 17:33:25 +0100

    BBCode - fixed syntax error

--- src/Content/Text/BBCode.php
+++ src/Content/Text/BBCode.php
@@ -1364,8 +1364,8 @@ class BBCode extends BaseObject
 		// We need no target="_blank" for local links
 		// convert links start with System::baseUrl() as local link without the target="_blank" attribute
 		$escapedBaseUrl = preg_quote(System::baseUrl(), '/');
-		$text = preg_replace("/\[url\]($escapedBaseUrl[$URLSearchString]*)\[\/url\]/ism", '<a href="$1">$1</a>', $text);
-		$text = preg_replace("/\[url\=($escapedBaseUrl[$URLSearchString]*)\](.*?)\[\/url\]/ism", '<a href="$1">$2</a>', $text);		
+		$text = preg_replace("/\[url\](".$escapedBaseUrl."[$URLSearchString]*)\[\/url\]/ism", '<a href="$1">$1</a>', $text);
+		$text = preg_replace("/\[url\=(".$escapedBaseUrl."[$URLSearchString]*)\](.*?)\[\/url\]/ism", '<a href="$1">$2</a>', $text);		
 
 		$text = preg_replace("/\[url\]([$URLSearchString]*)\[\/url\]/ism", '<a href="$1" target="_blank">$1</a>', $text);
 		$text = preg_replace("/\[url\=([$URLSearchString]*)\](.*?)\[\/url\]/ism", '<a href="$1" target="_blank">$2</a>', $text);
