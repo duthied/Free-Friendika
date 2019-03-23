fb13d0c31bd656975273909cb9c421642a81764a
Hypolite Petovan <hypolite@mrpetovan.com>
2019-03-23 10:02:32 -0400

    Add tests for api_status_show
    - Update test for api_get_last_status()
    - Add test class properties declaration

--- tests/include/ApiTest.php
+++ tests/include/ApiTest.php
@@ -31,6 +31,18 @@ class ApiTest extends DatabaseTest
 	 */
 	protected $logOutput;
 
+	/** @var App */
+	protected $app;
+
+	/** @var array */
+	protected $selfUser;
+	/** @var array */
+	protected $friendUser;
+	/** @var array */
+	protected $otherUser;
+
+	protected $wrongUserId;
+
 	/**
 	 * Create variables used by tests.
 	 */
@@ -1272,37 +1284,30 @@ class ApiTest extends DatabaseTest
 
 	/**
 	 * Test the api_status_show() function.
-	 * @return void
 	 */
 	public function testApiStatusShowWithJson()
 	{
-		$this->markTestSkipped('This test requires an item ID');
-
-		$result = api_status_show('json');
+		$result = api_status_show('json', 1);
 		$this->assertStatus($result['status']);
 	}
 
 	/**
 	 * Test the api_status_show() function with an XML result.
-	 * @return void
 	 */
 	public function testApiStatusShowWithXml()
 	{
-		$this->markTestSkipped('This test requires an item ID');
-
-		$result = api_status_show('xml');
+		$result = api_status_show('xml', 1);
 		$this->assertXml($result, 'statuses');
 	}
 
 	/**
-	 * Test the api_status_show() function with a raw result.
-	 * @return void
+	 * Test the api_get_last_status() function
 	 */
 	public function testApiGetLastStatus()
 	{
-		$user_info = api_get_user($this->app);
+		$item = api_get_last_status($this->selfUser['id'], $this->selfUser['id']);
 
-		$this->assertStatus(api_get_last_status($user_info['pid'], $user_info['uid']));
+		$this->assertNotNull($item);
 	}
 
 	/**
