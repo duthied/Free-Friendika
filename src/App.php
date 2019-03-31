16f44a8237ca0ba0ceea47696cf3a6be306e575d
Tobias Diekershoff <tobias.diekershoff@gmx.net>
2019-03-31 08:53:25 +0200

    dont check but only copy if BasePath is present

--- src/App.php
+++ src/App.php
@@ -182,13 +182,11 @@ class App
 	 */
 	public function registerStylesheet($path)
 	{
-		if (!empty($this->getBasePath())) {
-			$url = str_replace($this->getBasePath() . DIRECTORY_SEPARATOR, '', $path);
-		} else {
-			$url = $path;
+		if (mb_strpos($path, $this->getBasePath() . DIRECTORY_SEPARATOR) == 0) {
+				$path = mb_substr($path, mb_strlen($this->getBasePath() . DIRECTORY_SEPARATOR));
 		}
 
-		$this->stylesheets[] = trim($url, '/');
+		$this->stylesheets[] = trim($path, '/');
 	}
 
 	/**
