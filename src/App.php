3de1fc3097cc7a1e38bf1a0c3cae5ae4ec8067eb
Hypolite Petovan <hypolite@mrpetovan.com>
2019-03-31 06:00:20 -0400

    Hotfix for App->registerStylesheet
    - Wrong condition for strpos()
    - Follow-up to #6953

--- src/App.php
+++ src/App.php
@@ -182,8 +182,8 @@ class App
 	 */
 	public function registerStylesheet($path)
 	{
-		if (mb_strpos($path, $this->getBasePath() . DIRECTORY_SEPARATOR) == 0) {
-				$path = mb_substr($path, mb_strlen($this->getBasePath() . DIRECTORY_SEPARATOR));
+		if (mb_strpos($path, $this->getBasePath() . DIRECTORY_SEPARATOR) === 0) {
+			$path = mb_substr($path, mb_strlen($this->getBasePath() . DIRECTORY_SEPARATOR));
 		}
 
 		$this->stylesheets[] = trim($path, '/');
