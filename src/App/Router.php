b2ff31da7416f9809ac9e83010b0a90305b468d4
Philipp Holzer <admin@philipp.info>
2019-05-02 22:17:09 +0200

    Move mod/bookmarklet to src/Module/BookMarklet

--- src/App/Router.php
+++ src/App/Router.php
@@ -52,6 +52,7 @@ class Router
 		$this->routeCollector->addRoute(['GET'],         '/apps',                Module\Apps::class);
 		$this->routeCollector->addRoute(['GET'],         '/attach/{item:\d+}',   Module\Attach::class);
 		$this->routeCollector->addRoute(['GET'],         '/babel',               Module\Babel::class);
+		$this->routeCollector->addRoute(['GET'],         '/bookmarklet',         Module\BookMarklet::class);
 		$this->routeCollector->addGroup('/contact', function (RouteCollector $collector) {
 			$collector->addRoute(['GET'], '[/]',                                 Module\Contact::class);
 			$collector->addRoute(['GET'], '/{id:\d+}[/posts|conversations]',     Module\Contact::class);
