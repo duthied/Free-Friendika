6c341962648b73c935c2de26996b07332b776a1f
Michael Vogel <icarus@dabo.de>
2019-05-03 06:17:26 +0200

    Cleaning confirm

--- mod/dfrn_confirm.php
+++ mod/dfrn_confirm.php
@@ -24,7 +24,6 @@ use Friendica\Core\Logger;
 use Friendica\Core\Protocol;
 use Friendica\Core\System;
 use Friendica\Database\DBA;
-use Friendica\Model\APContact;
 use Friendica\Model\Contact;
 use Friendica\Model\Group;
 use Friendica\Model\User;
@@ -145,11 +144,8 @@ function dfrn_confirm_post(App $a, $handsfree = null)
 		}
 
 		// an empty DFRN-ID tells us that it had been a request via AP from a Friendica contact
-		if (($network === Protocol::DFRN) && empty($dfrn_id) && !empty($contact['hub-verify'])) {
-			$apcontact = APContact::getByURL($contact['url']);
-			if (!empty($apcontact)) {
-				$network = Protocol::ACTIVITYPUB;
-			}
+		if (($network === Protocol::DFRN) && empty($dfrn_id)) {
+			$network = Contact::getProtocol($contact['url'], $contact['network']);
 		}
 
 		if ($network === Protocol::DFRN) {
