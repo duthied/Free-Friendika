4704bf6394d97185c46a39992d920479ef5f0273
Philipp Holzer <admin+github@philipp.info>
2019-10-23 00:15:17 +0200

    Remove unused function return_bytes

--- include/text.php
+++ include/text.php
@@ -209,20 +209,6 @@ function get_cats_and_terms($item)
 	return [$categories, $folders];
 }
 
-/**
- * return number of bytes in size (K, M, G)
- * @param string $size_str
- * @return int
- */
-function return_bytes($size_str) {
-	switch (substr ($size_str, -1)) {
-		case 'M': case 'm': return (int)$size_str * 1048576;
-		case 'K': case 'k': return (int)$size_str * 1024;
-		case 'G': case 'g': return (int)$size_str * 1073741824;
-		default: return $size_str;
-	}
-}
-
 /// @TODO Rewrite this
 function is_a_date_arg($s) {
 	$i = intval($s);
