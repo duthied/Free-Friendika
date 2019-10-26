5d995b854a77e7d8a3279e09d6d158db7eb9644d
Philipp Holzer <admin+github@philipp.info>
2019-10-26 05:24:05 +0200

    just remove parenthess

--- mod/notifications.php
+++ mod/notifications.php
@@ -78,7 +78,7 @@ function notifications_content(App $a)
 	}
 
 	$page = ($_REQUEST['page'] ?? 0) ?: 1;
-	$show =  isset($_REQUEST['show']) ? $_REQUEST['show'] : false;
+	$show = ($_REQUEST['show'] ?? '') === 'all';
 
 	Nav::setSelected('notifications');
 
