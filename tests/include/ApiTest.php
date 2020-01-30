940e4c83cbc55169ab501aada450c22a299b373a
Hypolite Petovan <hypolite@mrpetovan.com>
2020-01-29 23:14:34 -0500

    Remove obsolete tests

--- tests/include/ApiTest.php
+++ tests/include/ApiTest.php
@@ -3897,58 +3897,6 @@ XML;
 	}
 
 	/**
-	 * Test the api_friendica_profile_show() function.
-	 *
-	 * @return void
-	 */
-	public function testApiFriendicaProfileShow()
-	{
-		$result = api_friendica_profile_show('json');
-		// We can't use assertSelfUser() here because the user object is missing some properties.
-		$this->assertEquals($this->selfUser['id'], $result['$result']['friendica_owner']['cid']);
-		$this->assertEquals('DFRN', $result['$result']['friendica_owner']['location']);
-		$this->assertEquals($this->selfUser['name'], $result['$result']['friendica_owner']['name']);
-		$this->assertEquals($this->selfUser['nick'], $result['$result']['friendica_owner']['screen_name']);
-		$this->assertEquals('dfrn', $result['$result']['friendica_owner']['network']);
-		$this->assertTrue($result['$result']['friendica_owner']['verified']);
-		$this->assertFalse($result['$result']['multi_profiles']);
-	}
-
-	/**
-	 * Test the api_friendica_profile_show() function with a profile ID.
-	 *
-	 * @return void
-	 */
-	public function testApiFriendicaProfileShowWithProfileId()
-	{
-		$this->markTestIncomplete('We need to add a dataset for this.');
-	}
-
-	/**
-	 * Test the api_friendica_profile_show() function with a wrong profile ID.
-	 *
-	 * @return void
-	 * @expectedException Friendica\Network\HTTPException\BadRequestException
-	 */
-	public function testApiFriendicaProfileShowWithWrongProfileId()
-	{
-		$_REQUEST['profile_id'] = 666;
-		api_friendica_profile_show('json');
-	}
-
-	/**
-	 * Test the api_friendica_profile_show() function without an authenticated user.
-	 *
-	 * @return void
-	 * @expectedException Friendica\Network\HTTPException\ForbiddenException
-	 */
-	public function testApiFriendicaProfileShowWithoutAuthenticatedUser()
-	{
-		$_SESSION['authenticated'] = false;
-		api_friendica_profile_show('json');
-	}
-
-	/**
 	 * Test the api_saved_searches_list() function.
 	 *
 	 * @return void
