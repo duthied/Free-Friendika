bfea5d42ad3beb014c2d2d323549f07faa028360
Hypolite Petovan <hypolite@mrpetovan.com>
2020-02-23 20:35:36 -0500

    Simplify about field conversion to plaintext in Protocol\Diaspora

--- src/Protocol/Diaspora.php
+++ src/Protocol/Diaspora.php
@@ -4135,8 +4135,7 @@ class Diaspora
 				$dob = DateTimeFormat::utc($year . '-' . $month . '-'. $day, 'Y-m-d');
 			}
 
-			$about = $profile['about'];
-			$about = strip_tags(BBCode::convert($about));
+			$about = BBCode::toPlaintext($profile['about']);
 
 			$location = Profile::formatLocation($profile);
 			$tags = '';
