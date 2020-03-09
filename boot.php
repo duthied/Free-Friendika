416b0b814e888311b499ce6e72a9508db4b3f1ca
Tobias Diekershoff <tobias.diekershoff@gmx.net>
2020-03-09 06:43:37 +0100

    2020.03-rc

--- boot.php
+++ boot.php
@@ -39,7 +39,7 @@ use Friendica\Util\DateTimeFormat;
 
 define('FRIENDICA_PLATFORM',     'Friendica');
 define('FRIENDICA_CODENAME',     'Dalmatian Bellflower');
-define('FRIENDICA_VERSION',      '2020.03-dev');
+define('FRIENDICA_VERSION',      '2020.03-rc');
 define('DFRN_PROTOCOL_VERSION',  '2.23');
 define('NEW_UPDATE_ROUTINE_VERSION', 1170);
 
