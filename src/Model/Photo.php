7863406080bf4ea1ca73d6ffd49aac315b447b31
Michael <heluecht@pirati.ca>
2020-03-12 06:20:49 +0000

    Prevent "null" permissions on photos

--- src/Model/Photo.php
+++ src/Model/Photo.php
@@ -298,6 +298,13 @@ class Photo
 			$backend_ref = $storage->put($Image->asString(), $backend_ref);
 		}
 
+		// Prevent "null" permissions
+		if (!empty($uid)) {
+			$allow_cid = $allow_cid ?? '<' . $uid . '>';
+			$allow_gid = $allow_gid ?? '';
+			$deny_cid = $deny_cid ?? '';
+			$deny_gid = $deny_gid ?? '';
+		}
 
 		$fields = [
 			"uid" => $uid,
