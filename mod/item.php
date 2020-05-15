dfb75e16e18409cdd5012c9b975bc2d9cf29445c
Michael <heluecht@pirati.ca>
2020-05-15 20:24:38 +0000

    Reverting stuff

--- mod/item.php
+++ mod/item.php
@@ -720,6 +720,7 @@ function item_post(App $a) {
 		if ($return_path) {
 			DI::baseUrl()->redirect($return_path);
 		}
+
 		throw new HTTPException\InternalServerErrorException(DI::l10n()->t('Item wasn\'t stored.'));
 	}
 
