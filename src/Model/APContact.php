21ab11ac18ba5593bab7f2bf37be5212f91e27da
Michael <heluecht@pirati.ca>
2020-05-22 11:29:58 +0000

    Only fetch the gsid when it had been empty before

--- src/Model/APContact.php
+++ src/Model/APContact.php
@@ -291,8 +291,10 @@ class APContact
 			$apcontact['baseurl'] = null;
 		}
 
-		if (!empty($apcontact['baseurl'])) {
+		if (!empty($apcontact['baseurl']) && empty($fetched_contact['gsid'])) {
 			$apcontact['gsid'] = GServer::getID($apcontact['baseurl']);
+		} elseif (!empty($fetched_contact['gsid'])) {
+			$apcontact['gsid'] = $fetched_contact['gsid'];
 		}
 
 		if ($apcontact['url'] == $apcontact['alias']) {
