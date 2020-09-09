5c3cc76acffa20545a76061f38354855f7513385
Michael Vogel <icarus@dabo.de>
2020-09-09 20:03:14 +0200

    Update include/conversation.php
    Co-authored-by: Hypolite Petovan <hypolite@mrpetovan.com>

--- include/conversation.php
+++ include/conversation.php
@@ -742,13 +742,14 @@ function conversation_fetch_comments($thread_items, $pinned) {
 		}
 
 		if (($row['gravity'] == GRAVITY_PARENT) && !$row['origin'] && ($row['author-id'] == $row['owner-id'])) {
-			if (!Contact::isSharing($row['author-id'], $row['uid'])) {
+			if (Contact::isSharing($row['author-id'], $row['uid'])) {
+				$row['direction'] = ['direction' => 6, 'title' => DI::l10n()->t('You are following %s.', $row['author-name'])];
+			} else {
 				if ($row['post-type'] == Item::PT_TAG) {
 					$row['direction'] = ['direction' => 4, 'title' => DI::l10n()->t('Tagged')];
 				}
 				$parentlines[] = $lineno;
-			} else {
-				$row['direction'] = ['direction' => 6, 'title' => DI::l10n()->t('You are following %s.', $row['author-name'])];
+				
 			}
 		}
 
