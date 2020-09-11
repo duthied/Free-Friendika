b5b2eaf5136cb7c71d222493893458294a84e5ee
Hypolite Petovan <hypolite@mrpetovan.com>
2020-09-10 20:11:53 -0400

    [frio] Disable custom background image for light/dark/black schemes

--- view/theme/frio/scheme/light.php
+++ view/theme/frio/scheme/light.php
@@ -3,7 +3,7 @@
  * Name: Light
  * Licence: AGPL
  * Author: Hypolite Petovan <hypolite@friendica.mrpetovan.com>
- * Overwrites: nav_bg, nav_icon_color, link_color, background_color, login_bg_color, contentbg_transp
+ * Overwrites: nav_bg, nav_icon_color, link_color, background_color, background_image, login_bg_color, contentbg_transp
  * Accented: yes
  */
 
@@ -18,3 +18,4 @@ $link_color = '#' . $accentColor->getHex();
 $background_color = "#ededed";
 $login_bg_color = "#ededed";
 $contentbg_transp = 100;
+$background_image = '';
