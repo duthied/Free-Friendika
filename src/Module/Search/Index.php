c8f385727e11dcce6b4cb4ae75f3e756b4fb8b72
Hypolite Petovan <hypolite@mrpetovan.com>
2020-09-20 11:19:03 -0400

    Improve log message language in Module/Search/Index.php
    Co-authored-by: Steffen K9 <admin@libranet.de>

--- src/Module/Search/Index.php
+++ src/Module/Search/Index.php
@@ -135,7 +135,7 @@ class Index extends BaseSearch
 		// Don't perform a fulltext or tag search on search results that look like an URL
 		// Tags don't look like an URL and the fulltext search does only work with natural words
 		if (parse_url($search, PHP_URL_SCHEME) && parse_url($search, PHP_URL_HOST)) {
-			Logger::info('Skipping tag and fulltext search since the search looks like an URL.', ['q' => $search]);
+			Logger::info('Skipping tag and fulltext search since the search looks like a URL.', ['q' => $search]);
 			notice(DI::l10n()->t('No results.'));
 			return $o;
 		}
