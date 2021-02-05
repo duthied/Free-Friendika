3446829a19d74b7be51344660b74715e5de1b7dd
Michael <heluecht@pirati.ca>
2021-02-05 06:41:34 +0000

    And again ...

--- include/api.php
+++ include/api.php
@@ -1821,11 +1821,11 @@ function api_statuses_networkpublic_timeline($type)
 	$params = ['order' => ['id' => true], 'limit' => [$start, $count]];
 	$statuses = Post::toArray(Post::selectForUser(api_user(), Item::DISPLAY_FIELDLIST, $condition, $params));
 if (empty($statuses)) {
-	return ['test' => $statuses, 'condition' => $condition, 'params' => $params, 'db' => DBA::errorNo(), 'msg' => DBA::errorMessage()];
+	return ['status' => [], 'statuses' => $statuses, 'condition' => $condition, 'params' => $params, 'db' => DBA::errorNo(), 'msg' => DBA::errorMessage()];
 }
 	$ret = api_format_items($statuses, $user_info, false, $type);
 if (empty($ret)) {
-	return ['ret' => $ret, 'test' => $statuses, 'condition' => $condition, 'params' => $params, 'db' => DBA::errorNo(), 'msg' => DBA::errorMessage()];
+	return ['status' => [], 'ret' => $ret, 'statuses' => $statuses, 'condition' => $condition, 'params' => $params, 'db' => DBA::errorNo(), 'msg' => DBA::errorMessage()];
 }
 	bindComments($ret);
 
