2072fb1eed2dbadcd9587e44eef5c855093212b1
Michael <heluecht@pirati.ca>
2021-03-24 19:47:55 +0000

    Avoid foreign key violations when deleting unused tables

--- src/Database/DBStructure.php
+++ src/Database/DBStructure.php
@@ -78,9 +78,9 @@ class DBStructure
 			return;
 		}
 
-		$old_tables = ['fserver', 'gcign', 'gcontact', 'gcontact-relation', 'gfollower' ,'glink', 'item', 'item-delivery-data',
+		$old_tables = ['fserver', 'gcign', 'gcontact', 'gcontact-relation', 'gfollower' ,'glink', 'item-delivery-data',
 			'item-activity', 'item-content', 'item_id', 'participation', 'poll', 'poll_result', 'queue', 'retriever_rule',
-			'sign', 'spam', 'term', 'thread', 'user-item'];
+			'sign', 'spam', 'term', 'user-item', 'thread', 'item'];
 
 		$tables = DBA::selectToArray(['INFORMATION_SCHEMA' => 'TABLES'], ['TABLE_NAME'],
 			['TABLE_SCHEMA' => DBA::databaseName(), 'TABLE_TYPE' => 'BASE TABLE']);
