19a86fd8f293acbe770f1a4ddcd753b170cd6a40
Matthew Exon <git.mexon@spamgourmet.com>
2021-05-02 16:14:39 +0200

    Fix console user config set command

--- src/Console/User.php
+++ src/Console/User.php
@@ -49,10 +49,6 @@ class User extends \Asika\SimpleConsole\Console
 	 */
 	private $l10n;
 	/**
-	 * @var Database
-	 */
-	private $dba;
-	/**
 	 * @var IPConfig
 	 */
 	private $pConfig;
@@ -99,7 +95,6 @@ HELP;
 
 		$this->appMode = $appMode;
 		$this->l10n    = $l10n;
-		$this->dba     = $dba;
 		$this->pConfig = $pConfig;
 	}
 
@@ -176,15 +171,15 @@ HELP;
 	 *
 	 * @param int $arg_index Index of the nick argument in the arguments list
 	 *
-	 * @return mixed user data or dba failure result
+	 * @return array|boolean User record with uid field, or false if user is not found
 	 * @throws \Friendica\Network\HTTPException\InternalServerErrorException
 	 */
 	private function getUserByNick($arg_index)
 	{
 		$nick = $this->getNick($arg_index);
 
-		$user = $this->dba->selectFirst('user', ['uid'], ['nickname' => $nick]);
-		if (!$this->dba->isResult($user)) {
+		$user = UserModel::getByNickname($nick, ['uid']);
+		if (!$user) {
 			throw new RuntimeException($this->l10n->t('User not found'));
 		}
 
@@ -212,7 +207,7 @@ HELP;
 		try {
 			$result = UserModel::updatePassword($user['uid'], $password);
 
-			if (!$this->dba->isResult($result)) {
+			if (!$result) {
 				throw new \Exception($this->l10n->t('Password update failed. Please try again.'));
 			}
 
@@ -431,7 +426,9 @@ HELP;
 				return false;
 		}
 
-		$table->addRow($user);
+		if ($user) {
+			$table->addRow($user);
+		}
 		$this->out($table->getTable());
 
 		return true;
@@ -489,7 +486,7 @@ HELP;
 					throw new RuntimeException('Key does not exist');
 				}
 
-				$this->out($pconfig->get($user['uid'], $category, $key));
+				$this->out($this->pConfig->get($user['uid'], $category, $key));
 				break;
 			case 'set':
 				$value = $this->getArgument(5);
@@ -508,7 +505,7 @@ HELP;
 					throw new RuntimeException('Value not changed');
 				}
 
-				$pconfig->set($user['uid'], $category, $key, $value);
+				$this->pConfig->set($user['uid'], $category, $key, $value);
 				break;
 			case 'delete':
 				if (!array_key_exists($category, $values)) {
@@ -518,7 +515,7 @@ HELP;
 					throw new RuntimeException('Key does not exist');
 				}
 
-				$pconfig->delete($user['uid'], $category, $key);
+				$this->pConfig->delete($user['uid'], $category, $key);
 				break;
 			default:
 				$this->out($this->getHelp());
