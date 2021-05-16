1bcfa714a1006f31ba1a03063cb44cec9fe7bc98
Philipp <admin@philipp.info>
2021-05-16 23:22:44 +0200

    Make tests PHPUnit 6.0 compatible

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -32,7 +32,7 @@ require __DIR__ . '/../vendor/autoload.php';
 
 // Backward compatibility
 if (!class_exists(TestCase::class)) {
-	class_alias(PHPUnit_Framework_TestCase::class, TestCase::class);
+	class_alias(\PHPUnit\Framework\TestCase::class, TestCase::class);
 }
 
 $dice = new Dice();
