ed4a18f079ff8b92e111df477cd26a49ed22e18e
Michael <heluecht@pirati.ca>
2021-06-25 17:03:35 +0000

    Function to return the avatar and header path for a given contact id

--- src/Object/Api/Mastodon/Account.php
+++ src/Object/Api/Mastodon/Account.php
@@ -26,7 +26,6 @@ use Friendica\BaseDataTransferObject;
 use Friendica\Collection\Api\Mastodon\Fields;
 use Friendica\Content\Text\BBCode;
 use Friendica\Database\DBA;
-use Friendica\DI;
 use Friendica\Model\Contact;
 use Friendica\Util\DateTimeFormat;
 
@@ -113,9 +112,9 @@ class Account extends BaseDataTransferObject
 
 		$this->note            = BBCode::convert($publicContact['about'], false);
 		$this->url             = $publicContact['url'];
-		$this->avatar          = (($userContact['photo'] ?? '') ?: $publicContact['photo']) ?: DI::baseUrl() . '/photo/contact/'. (($userContact['id'] ?? 0) ?: $publicContact['id']);
+		$this->avatar          = (($userContact['photo'] ?? '') ?: $publicContact['photo']) ?: Contact::getAvatarForContact(($userContact['id'] ?? 0) ?: $publicContact['id']);
 		$this->avatar_static   = $this->avatar;
-		$this->header          = DI::baseUrl() . '/photo/header/'. (($userContact['id'] ?? 0) ?: $publicContact['id']);
+		$this->header          = Contact::getHeaderForContact(($userContact['id'] ?? 0) ?: $publicContact['id']);
 		$this->header_static   = $this->header;
 		$this->followers_count = $apcontact['followers_count'] ?? 0;
 		$this->following_count = $apcontact['following_count'] ?? 0;
