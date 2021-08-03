ec3eead034e18365466237f2db71ad959cd8b7e0
Michael <heluecht@pirati.ca>
2021-08-03 05:39:04 +0000

    Don't set the pconfig values on manual mode

--- src/Protocol/Feed.php
+++ src/Protocol/Feed.php
@@ -601,7 +601,9 @@ class Feed
 				}
 				$publish_at = date(DateTimeFormat::MYSQL, $publish_time);
 
-				Post\Delayed::add($posting['item']['uri'], $posting['item'], $posting['notify'], false, $publish_at, $posting['taglist'], $posting['attachments']);
+				if (Post\Delayed::add($posting['item']['uri'], $posting['item'], $posting['notify'], false, $publish_at, $posting['taglist'], $posting['attachments'])) {
+					DI::pConfig()->set($item['uid'], 'system', 'last_publish', $publish_time);
+				}
 			}
 		}
 
