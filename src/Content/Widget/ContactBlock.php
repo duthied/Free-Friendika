325342a8869cf0a9eb0ca9c8fc8bd96e37aba2a7
Michael <heluecht@pirati.ca>
2021-09-19 17:56:44 +0000

    Renamed variable

--- src/Content/Widget/ContactBlock.php
+++ src/Content/Widget/ContactBlock.php
@@ -99,10 +99,10 @@ class ContactBlock
 				'network' => Protocol::FEDERATED,
 			], ['limit' => $shown]);
 
-			$contact_ids = array_column($personal_contacts, 'uri-id');
+			$contact_uriids = array_column($personal_contacts, 'uri-id');
 
-			if (!empty($contact_ids)) {
-				$contacts_stmt = DBA::select('contact', ['id', 'uid', 'addr', 'url', 'name', 'thumb', 'avatar', 'network'], ['uri-id' => $contact_ids, 'uid' => $contact_uid]);
+			if (!empty($contact_uriids)) {
+				$contacts_stmt = DBA::select('contact', ['id', 'uid', 'addr', 'url', 'name', 'thumb', 'avatar', 'network'], ['uri-id' => $contact_uriids, 'uid' => $contact_uid]);
 
 				if (DBA::isResult($contacts_stmt)) {
 					$contacts_title = DI::l10n()->tt('%d Contact', '%d Contacts', $total);
