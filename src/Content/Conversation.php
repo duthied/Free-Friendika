e08c5ca2e07b0bfe70aa66cbc64e6bfb90e6ad7c
Michael <heluecht@pirati.ca>
2021-09-26 04:16:21 +0000

    Another try ...

--- src/Content/Conversation.php
+++ src/Content/Conversation.php
@@ -285,15 +285,15 @@ class Conversation
 		$this->profiler->startRecording('rendering');
 		$o = '';
 
-		$x['allow_location']   = $x['allow_location'] ?? $user['allow_location'];
+		$x['allow_location']   = $x['allow_location']   ?? $user['allow_location'];
 		$x['default_location'] = $x['default_location'] ?? $user['default-location'];
-		$x['nickname']         = $x['nickname'] ?? $user['nickname'];
-		$x['lockstate']        = $x['lockstate'] ?? ACL::getLockstateForUserId($user['uid']) ? 'lock' : 'unlock';
-		$x['acl']              = $x['acl'] ?? ACL::getFullSelectorHTML($this->page, $user['uid'], true);
-		$x['bang']             = $x['bang'] ?? '';
-		$x['visitor']          = $x['visitor'] ?? 'block';
-		$x['is_owner']         = $x['is_owner'] ?? true;
-		$x['profile_uid']      = $x['profile_uid'] ?? local_user();
+		$x['nickname']         = $x['nickname']         ?? $user['nickname'];
+		$x['lockstate']        = $x['lockstate']        ?? ACL::getLockstateForUserId($user['uid']) ? 'lock' : 'unlock';
+		$x['acl']              = $x['acl']              ?? ACL::getFullSelectorHTML($this->page, $user['uid'], true);
+		$x['bang']             = $x['bang']             ?? '';
+		$x['visitor']          = $x['visitor']          ?? 'block';
+		$x['is_owner']         = $x['is_owner']         ?? true;
+		$x['profile_uid']      = $x['profile_uid']      ?? local_user();
 
 
 		$geotag = !empty($x['allow_location']) ? Renderer::replaceMacros(Renderer::getMarkupTemplate('jot_geotag.tpl'), []) : '';
