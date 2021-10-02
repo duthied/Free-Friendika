e938dc1fef281710d2bedc99c770346336ee26bf
Michael <heluecht@pirati.ca>
2021-10-02 11:59:03 +0000

    Updated "database.sql"

--- database.sql
+++ database.sql
@@ -1,5 +1,5 @@
 -- ------------------------------------------
--- Friendica 2021.09-rc (Siberian Iris)
+-- Friendica 2021.12-dev (Siberian Iris)
 -- DB_UPDATE_VERSION 1435
 -- ------------------------------------------
 
