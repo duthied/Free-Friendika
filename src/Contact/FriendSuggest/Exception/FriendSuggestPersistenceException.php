92c7b99a4f67e7b6ea280aa3484b45cbd53cd679
Philipp <admin+Github@philipp.info>
2021-10-23 06:49:32 +0200

    Update src/Contact/FriendSuggest/Exception/FriendSuggestPersistenceException.php
    Co-authored-by: Hypolite Petovan <hypolite@mrpetovan.com>

--- src/Contact/FriendSuggest/Exception/FriendSuggestPersistenceException.php
+++ src/Contact/FriendSuggest/Exception/FriendSuggestPersistenceException.php
@@ -23,7 +23,7 @@ namespace Friendica\Contact\FriendSuggest\Exception;
 
 class FriendSuggestPersistenceException extends \RuntimeException
 {
-	public function __construct($message = "", \Throwable $previous = null)
+	public function __construct($message = '', \Throwable $previous = null)
 	{
 		parent::__construct($message, 500, $previous);
 	}
