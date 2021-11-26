a3e3d87a420d24518c5d74bc2b1bb7ee3474490b
Michael <heluecht@pirati.ca>
2021-11-26 21:57:18 +0000

    Standards

--- src/Module/Api/Twitter/Friendships/Incoming.php
+++ src/Module/Api/Twitter/Friendships/Incoming.php
@@ -37,17 +37,17 @@ class Incoming extends ContactEndpoint
 		$uid = BaseApi::getCurrentUserID();
 
 		// Expected value for user_id parameter: public/user contact id
-		$cursor        = filter_input(INPUT_GET, 'cursor'               , FILTER_VALIDATE_INT, ['options' => ['default' => -1]]);
+		$cursor        = filter_input(INPUT_GET, 'cursor'       , FILTER_VALIDATE_INT, ['options' => ['default' => -1]]);
 		$stringify_ids = filter_input(INPUT_GET, 'stringify_ids', FILTER_VALIDATE_BOOLEAN, ['options' => ['default' => false]]);
-		$count         = filter_input(INPUT_GET, 'count'                , FILTER_VALIDATE_INT, ['options' => [
-			'default' => self::DEFAULT_COUNT,
+		$count         = filter_input(INPUT_GET, 'count'        , FILTER_VALIDATE_INT, ['options' => [
+			'default'   => self::DEFAULT_COUNT,
 			'min_range' => 1,
 			'max_range' => self::MAX_COUNT,
 		]]);
 		// Friendica-specific
-		$since_id      = filter_input(INPUT_GET, 'since_id', FILTER_VALIDATE_INT);
-		$max_id        = filter_input(INPUT_GET, 'max_id'  , FILTER_VALIDATE_INT);
-		$min_id        = filter_input(INPUT_GET, 'min_id'  , FILTER_VALIDATE_INT);
+		$since_id = filter_input(INPUT_GET, 'since_id', FILTER_VALIDATE_INT);
+		$max_id   = filter_input(INPUT_GET, 'max_id'  , FILTER_VALIDATE_INT);
+		$min_id   = filter_input(INPUT_GET, 'min_id'  , FILTER_VALIDATE_INT);
 
 		$params = ['order' => ['cid' => true], 'limit' => $count];
 
