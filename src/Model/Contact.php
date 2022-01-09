8147073c47e38b20d14533b08dde8c258fa5eea1
Michael <heluecht@pirati.ca>
2022-01-09 03:29:16 +0000

    Added license information

--- src/Model/Contact.php
+++ src/Model/Contact.php
@@ -2235,7 +2235,8 @@ class Contact
 				case 'friendika':
 					/**
 					 * Picture credits
-					 * @author  Lostinlight <https://gitlab.com/lostinlight>
+					 * @author  Lostinlight <https://mastodon.xyz/@lightone>
+					 * @license CC0 https://creativecommons.org/share-your-work/public-domain/cc0/
 					 * @link    https://gitlab.com/lostinlight/per_aspera_ad_astra/-/blob/master/friendica-404/friendica-promo-bubbles.jpg
 					 */
 					$ret['header'] = DI::baseUrl() . '/images/friendica-banner.jpg';
