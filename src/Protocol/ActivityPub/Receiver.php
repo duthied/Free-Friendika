39a174536105de85b1224ce83466bb4c39c1041e
Michael <heluecht@pirati.ca>
2022-04-03 07:45:15 +0000

    Prevent the warning of an Undefined array key "object_object_type"

--- src/Protocol/ActivityPub/Receiver.php
+++ src/Protocol/ActivityPub/Receiver.php
@@ -733,7 +733,7 @@ class Receiver
 					in_array($object_data['object_object_type'], array_merge(['as:Tombstone'], self::CONTENT_TYPES))) {
 					ActivityPub\Processor::undoActivity($object_data);
 				} elseif (in_array($object_data['object_type'], array_merge(self::ACTIVITY_TYPES, ['as:Announce', 'as:Create', ''])) &&
-					($object_data['object_object_type'] == '')) {
+					empty($object_data['object_object_type'])) {
 					// We cannot detect the target object. So we can ignore it.
 				} elseif (in_array($object_data['object_type'], ['as:Create']) &&
 					in_array($object_data['object_object_type'], ['pt:CacheFile'])) {
