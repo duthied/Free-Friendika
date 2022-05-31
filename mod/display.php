f4e680c97e3856d1f96c0e5198c7bb2298c453e7
Michael <heluecht@pirati.ca>
2022-05-31 17:00:21 +0000

    Fix: Activities should be be able again on displayed posts

--- mod/display.php
+++ mod/display.php
@@ -106,7 +106,7 @@ function display_init(App $a)
 	}
 
 	if ($item['gravity'] != GRAVITY_PARENT) {
-		$parent = Post::selectFirstForUser($item_user, $fields, ['uid' => $item['uid'], 'uri-id' => $item['parent-uri-id']]);
+		$parent = Post::selectFirstForUser($item_user, $fields, ['uid' => [$item['uid'], local_user()], 'uri-id' => $item['parent-uri-id']], ['order' => ['uid']]);
 		$item = $parent ?: $item;
 	}
 
