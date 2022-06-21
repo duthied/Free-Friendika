ff9707713bc81d43f2e4744df3213a01555af069
Roland Häder <roland@mxchange.org>
2022-06-21 18:51:27 +0200

    Fixed: - Friendica\Content\BoundariesPager::renderFull(int $itemCount) must be   compatible with Friendica\Content\Pager::renderFull(int $itemCount): string

--- src/Content/BoundariesPager.php
+++ src/Content/BoundariesPager.php
@@ -130,6 +130,9 @@ class BoundariesPager extends Pager
 		return Renderer::replaceMacros($tpl, ['pager' => $data]);
 	}
 
+	/**
+	 * Unsupported method, must be type-compatible
+	 */
 	public function renderFull(int $itemCount): string
 	{
 		throw new \BadMethodCallException();
