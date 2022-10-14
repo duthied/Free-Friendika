b5d3651f3feb5cc040a7bf3f0fe59621f16bcbb3
Tobias Diekershoff <tobias.diekershoff@gmx.net>
2022-10-14 20:52:38 +0200

    bump version to 2022.12-dev

--- boot.php
+++ boot.php
@@ -31,7 +31,7 @@ use Friendica\Model\Contact;
 
 define('FRIENDICA_PLATFORM',     'Friendica');
 define('FRIENDICA_CODENAME',     'Giant Rhubarb');
-define('FRIENDICA_VERSION',      '2022.10');
+define('FRIENDICA_VERSION',      '2022.12-dev');
 define('DFRN_PROTOCOL_VERSION',  '2.23');
 define('NEW_TABLE_STRUCTURE_VERSION', 1288);
 
