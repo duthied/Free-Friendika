142b399c848a3ff5f3e0f452361d284a1b5dc135
Hypolite Petovan <hypolite@mrpetovan.com>
2022-10-24 17:56:05 -0400

    Reformat Module\Settings\UserExport
    - Import IHandleUserSessions and DbaDefinition object in constructor
    - Convert remaining double quotes to single quotes
    - Convert static methods to dynamic to use class properties
    - Remove unused POST permission from route

--- static/routes.config.php
+++ static/routes.config.php
@@ -574,7 +574,7 @@ return [
 			'/photo[/new]'         => [Module\Settings\Profile\Photo\Index::class, [R::GET, R::POST]],
 			'/photo/crop/{guid}'   => [Module\Settings\Profile\Photo\Crop::class,  [R::GET, R::POST]],
 		],
-		'/userexport[/{action}]' => [Module\Settings\UserExport::class,             [R::GET, R::POST]],
+		'/userexport[/{action}]' => [Module\Settings\UserExport::class,            [R::GET         ]],
 	],
 
 	'/network' => [
