198d38545d6b3e94182f654449c441fd18131109
Hypolite Petovan <hypolite@mrpetovan.com>
2022-11-04 14:14:00 -0400

    Remove trailing spaces from Module\Post\Share

--- src/Module/Post/Share.php
+++ src/Module/Post/Share.php
@@ -47,7 +47,7 @@ class Share extends \Friendica\BaseModule
 	public function __construct(Content\Item $contentItem, IHandleUserSessions $session, L10n $l10n, App\BaseURL $baseUrl, App\Arguments $args, LoggerInterface $logger, Profiler $profiler, Response $response, array $server, array $parameters = [])
 	{
 		parent::__construct($l10n, $baseUrl, $args, $logger, $profiler, $response, $server, $parameters);
-		
+
 		$this->session     = $session;
 		$this->contentItem = $contentItem;
 	}
