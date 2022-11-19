a91944af24c2a06725f853717eb31378e055b520
Hypolite Petovan <hypolite@mrpetovan.com>
2022-11-19 13:33:05 -0500

    Fix constructor compatibility with BaseSettings parent class
    - Address https://paste.metaversum.wtf/?fc75de3f8eba6e7d#813SFjJwWWaJERQyrEpGAVnL33xZpTRV4MZeKmzsAvK8

--- src/Module/Settings/TwoFactor/Verify.php
+++ src/Module/Settings/TwoFactor/Verify.php
@@ -29,6 +29,7 @@ use Friendica\App;
 use Friendica\Core\L10n;
 use Friendica\Core\PConfig\Capability\IManagePersonalConfigValues;
 use Friendica\Core\Renderer;
+use Friendica\Core\Session\Capability\IHandleUserSessions;
 use Friendica\DI;
 use Friendica\Module\BaseSettings;
 use Friendica\Module\Response;
@@ -47,9 +48,9 @@ class Verify extends BaseSettings
 	/** @var IManagePersonalConfigValues */
 	protected $pConfig;
 
-	public function __construct(L10n $l10n, App\BaseURL $baseUrl, App\Arguments $args, LoggerInterface $logger, Profiler $profiler, Response $response, IManagePersonalConfigValues $pConfig, array $server, array $parameters = [])
+	public function __construct(IManagePersonalConfigValues $pConfig, IHandleUserSessions $session, App\Page $page, L10n $l10n, App\BaseURL $baseUrl, App\Arguments $args, LoggerInterface $logger, Profiler $profiler, Response $response, array $server, array $parameters = [])
 	{
-		parent::__construct($l10n, $baseUrl, $args, $logger, $profiler, $response, $server, $parameters);
+		parent::__construct($session, $page, $l10n, $baseUrl, $args, $logger, $profiler, $response, $server, $parameters);
 
 		$this->pConfig = $pConfig;
 
