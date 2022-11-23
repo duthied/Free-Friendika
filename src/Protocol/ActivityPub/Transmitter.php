a8eabae12fd8e54c6b16fd6f306640701b8dae23
Hypolite Petovan <hypolite@mrpetovan.com>
2022-11-23 10:40:41 -0500

    Ensure $owner is set in ActivityPub\Transmitter::sendActivity
    - Address https://github.com/friendica/friendica/issues/11993#issuecomment-1322704863

--- src/Protocol/ActivityPub/Transmitter.php
+++ src/Protocol/ActivityPub/Transmitter.php
@@ -2031,6 +2031,10 @@ class Transmitter
 		}
 
 		$owner = User::getOwnerDataById($uid);
+		if (empty($owner)) {
+			Logger::warning('No user found for actor, aborting', ['uid' => $uid]);
+			return false;
+		}
 
 		if (empty($id)) {
 			$id = DI::baseUrl() . '/activity/' . System::createGUID();
