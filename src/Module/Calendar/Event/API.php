349436a77a3ff41b54c1828b90fcfad1a89aaab7
Hypolite Petovan <hypolite@mrpetovan.com>
2022-12-02 19:37:39 -0500

    Fix event start time not being properly converted to UTC
    - This was triggering unexpected time comparison errors

--- src/Module/Calendar/Event/API.php
+++ src/Module/Calendar/Event/API.php
@@ -152,7 +152,7 @@ class API extends BaseModule
 		$share     = intval($request['share'] ?? 0);
 		$isPreview = intval($request['preview'] ?? 0);
 
-		$start = DateTimeFormat::convert($strStartDateTime ?? DBA::NULL_DATETIME, $this->timezone);
+		$start = DateTimeFormat::convert($strStartDateTime ?? DBA::NULL_DATETIME, 'UTC', $this->timezone);
 		if (!$noFinish) {
 			$finish = DateTimeFormat::convert($strFinishDateTime ?? DBA::NULL_DATETIME, 'UTC', $this->timezone);
 		} else {
