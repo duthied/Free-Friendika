9778b60e64974eaf51aa45a2659cbe53993239a7
Marek Bachmann <marek.bachmann@comtec.eecs.uni-kassel.de>
2022-12-06 01:14:15 +0100

    cleaned up my git mess :-/ anyways, should work now

--- src/Model/Item.php
+++ src/Model/Item.php
@@ -3134,44 +3134,6 @@ class Item
 		return $hook_data['html'];
 	}
 
-	private function make_image_grid(array &$data)
-	{
-		$item = $data['item'];
-		if ($item['has-media']) {
-			$attachments = Post\Media::splitAttachments($item['uri-id'], [], $item['has-media'] ?? false);
-			if (count($attachments['visual']) > 1) {
-				$img_tags_fc = array();
-				$img_tags_sc = array();
-				$count = 0;
-				foreach ($attachments['visual'] as $attachment) {
-					$src_url = Post\Media::getUrlForId($attachment['id']);
-					$preview_url = Post\Media::getPreviewUrlForId($attachment['id'], ($attachment['width'] > $attachment['height']) ? Proxy::SIZE_MEDIUM : Proxy::SIZE_LARGE);
-					$img_tag = array(
-						'$image' => [
-							'src' => $src_url,
-							'preview' => $preview_url,
-							'attachment' => $attachment,
-					]);
-					if ($count % 2 == 0) {
-						$img_tags_fc[] = $img_tag;
-					} else {
-						$img_tags_sc[] = $img_tag;
-					}
-					++$count;
-				}
-
-				$img_grid = Renderer::replaceMacros(Renderer::getMarkupTemplate('content/image_grid.tpl'), [
-					'columns' => [
-						'fc' => $img_tags_fc,
-						'sc' => $img_tags_sc,
-					],
-				]);
-				return $img_grid;
-			}
-		}
-
-	}
-
 	/**
 	 * This function removes images at the very end of a post based on the assumption that this images are interpreted
 	 * as attachments
