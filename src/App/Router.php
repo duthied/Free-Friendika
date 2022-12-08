d9e69fc5431facc9c9beba28aa38b1ef7b8873d3
Hypolite Petovan <hypolite@mrpetovan.com>
2022-12-08 10:10:40 -0500

    Fix OPTIONS responses

--- src/App/Router.php
+++ src/App/Router.php
@@ -299,12 +299,12 @@ class Router
 			// Check if the HTTP method is OPTIONS and return the special Options Module with the possible HTTP methods
 			if ($this->args->getMethod() === static::OPTIONS) {
 				$this->moduleClass = Options::class;
-				$this->parameters  = ['allowedMethods' => $dispatcher->getOptions($cmd)];
+				$this->parameters[] = ['AllowedMethods' => $dispatcher->getOptions($cmd)];
 			} else {
 				$routeInfo = $dispatcher->dispatch($this->args->getMethod(), $cmd);
 				if ($routeInfo[0] === Dispatcher::FOUND) {
 					$this->moduleClass = $routeInfo[1];
-					$this->parameters[]  = $routeInfo[2];
+					$this->parameters[] = $routeInfo[2];
 				} else if ($routeInfo[0] === Dispatcher::METHOD_NOT_ALLOWED) {
 					throw new HTTPException\MethodNotAllowedException($this->l10n->t('Method not allowed for this module. Allowed method(s): %s', implode(', ', $routeInfo[1])));
 				} else {
