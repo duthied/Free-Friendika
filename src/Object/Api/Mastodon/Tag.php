4f0968a93ca0d2d484f211a4c6b40ca5b66957a1
Hank Grabowski <hankgrabowski@gmail.com>
2022-12-28 17:01:06 -0500

    Make tag 'Names' Field have case-sensitive version

--- src/Object/Api/Mastodon/Tag.php
+++ src/Object/Api/Mastodon/Tag.php
@@ -51,8 +51,8 @@ class Tag extends BaseDataTransferObject
 	 */
 	public function __construct(BaseURL $baseUrl, array $tag, array $history = [], bool $following = false)
 	{
-		$this->name      = strtolower($tag['name']);
-		$this->url       = $baseUrl . '/search?tag=' . urlencode($this->name);
+		$this->name      = $tag['name'];
+		$this->url       = $baseUrl . '/search?tag=' . urlencode(strtolower($this->name));
 		$this->history   = $history;
 		$this->following = $following;
 	}
