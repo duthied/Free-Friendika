51ab7f4fcb806d624f3973f6e85b595d3209ee2f
Philipp <admin@philipp.info>
2023-02-13 20:56:19 +0100

    code review

--- tests/FixtureTestTrait.php
+++ tests/FixtureTestTrait.php
@@ -1,4 +1,23 @@
 <?php
+/**
+ * @copyright Copyright (C) 2010-2023, the Friendica project
+ *
+ * @license GNU AGPL version 3 or any later version
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
 
 namespace Friendica\Test;
 
@@ -39,8 +58,7 @@ trait FixtureTestTrait
 			->addRules(include __DIR__ . '/../static/dependencies.config.php')
 			->addRule(ConfigFileManager::class, [
 				'instanceOf' => Config::class,
-				'call'       => [['createConfigFileManager', [$this->root->url(), $server,],
-								  Dice::CHAIN_CALL]]])
+				'call'       => [['createConfigFileManager', [$this->root->url(), $server,], Dice::CHAIN_CALL]]])
 			->addRule(Database::class, ['instanceOf' => StaticDatabase::class, 'shared' => true])
 			->addRule(IHandleSessions::class, ['instanceOf' => Memory::class, 'shared' => true, 'call' => null])
 			->addRule(Arguments::class, [
