d6c1e85b5fe1a110ece698434e4b2010c16cb2ac
Hank Grabowski <hankgrabowski@gmail.com>
2023-02-16 08:05:32 -0500

    Add blocked/ignored filters to Mastodon Timeline API Home and Tags endpoints

--- src/Module/Api/Mastodon/Timelines/Home.php
+++ src/Module/Api/Mastodon/Timelines/Home.php
@@ -90,6 +90,13 @@ class Home extends BaseApi
 			$condition = DBA::mergeConditions($condition, ['gravity' => Item::GRAVITY_PARENT]);
 		}
 
+		if (!empty($uid)) {
+			$condition = DBA::mergeConditions(
+				$condition,
+				["NOT `parent-author-id` IN (SELECT `cid` FROM `user-contact` WHERE `uid` = ? AND (`blocked` OR `ignored`) AND `cid` = `parent-author-id`)", $uid]
+			);
+		}
+
 		$items = Post::selectForUser($uid, ['uri-id'], $condition, $params);
 
 		$display_quotes = self::appSupportsQuotes();
