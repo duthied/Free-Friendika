40ee96512a3073e0d0063d5202b3551bd27abe4b
Hank Grabowski <hankgrabowski@gmail.com>
2023-02-17 17:42:00 -0500

    Add extension to Mastodon Status object for Friendica-specific things   - first entry is the title

---
+++ src/Object/Api/Mastodon/Status/FriendicaExtension.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * @copyright Copyright (C) 2010-2023, the Friendica project
+ *
+ * @license GNU AGPL version 3 or any later version
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+
+namespace Friendica\Object\Api\Mastodon\Status;
+
+use Friendica\BaseDataTransferObject;
+
+/**
+ * Class FriendicaExtension
+ *
+ * Additional fields on Mastodon Statuses for storing Friendica specific data
+ *
+ * @see https://docs.joinmastodon.org/entities/status
+ */
+class FriendicaExtension extends BaseDataTransferObject
+{
+	/** @var string */
+	protected $title;
+
+	/**
+	 * Creates a status count object
+	 *
+	 * @param string $title
+	 * @throws \Friendica\Network\HTTPException\InternalServerErrorException
+	 */
+	public function __construct(string $title)
+	{
+		$this->title = $title;
+	}
+
+	public function __get($name)
+	{
+		return $this->$name;
+	}
+}
