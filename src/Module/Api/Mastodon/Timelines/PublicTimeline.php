c33c624f83839477e918b8336f23bc65b2a71da4
Hank Grabowski <hankgrabowski@gmail.com>
2023-02-28 11:36:24 -0500

    Add new temporal paging to Mastodon Timeline Public and List endpoints

--- src/Module/Api/Mastodon/Timelines/PublicTimeline.php
+++ src/Module/Api/Mastodon/Timelines/PublicTimeline.php
@@ -30,6 +30,7 @@ use Friendica\Model\Item;
 use Friendica\Model\Post;
 use Friendica\Module\BaseApi;
 use Friendica\Network\HTTPException;
+use Friendica\Object\Api\Mastodon\TimelineOrderByTypes;
 
 /**
  * @see https://docs.joinmastodon.org/methods/timelines/
@@ -44,21 +45,23 @@ class PublicTimeline extends BaseApi
 		$uid = self::getCurrentUserID();
 
 		$request = $this->getRequest([
+			'max_id'          => null,  // Return results older than id
+			'since_id'        => null,  // Return results newer than id
+			'min_id'          => null,  // Return results immediately newer than id
+			'limit'           => 20,    // Maximum number of results to return. Defaults to 20.
 			'local'           => false, // Show only local statuses? Defaults to false.
 			'remote'          => false, // Show only remote statuses? Defaults to false.
 			'only_media'      => false, // Show only statuses with media attached? Defaults to false.
-			'max_id'          => 0,     // Return results older than this id
-			'since_id'        => 0,     // Return results newer than this id
-			'min_id'          => 0,     // Return results immediately newer than this id
-			'limit'           => 20,    // Maximum number of results to return. Defaults to 20.
 			'with_muted'      => false, // Pleroma extension: return activities by muted (not by blocked!) users.
 			'exclude_replies' => false, // Don't show comments
+			'friendica_order' => TimelineOrderByTypes::ID, // Sort order options (defaults to ID)
 		], $request);
-
-		$params = ['order' => ['uri-id' => true], 'limit' => $request['limit']];
 
 		$condition = ['gravity' => [Item::GRAVITY_PARENT, Item::GRAVITY_COMMENT], 'private' => Item::PUBLIC,
 			'network' => Protocol::FEDERATED, 'author-blocked' => false, 'author-hidden' => false];
+
+		$condition = $this->addPagingConditions($request, $condition);
+		$params = $this->buildOrderAndLimitParams($request);
 
 		if ($request['local']) {
 			$condition = DBA::mergeConditions($condition, ["`uri-id` IN (SELECT `uri-id` FROM `post-user` WHERE `origin`)"]);
@@ -73,19 +76,6 @@ class PublicTimeline extends BaseApi
 				Post\Media::AUDIO, Post\Media::IMAGE, Post\Media::VIDEO]);
 		}
 
-		if (!empty($request['max_id'])) {
-			$condition = DBA::mergeConditions($condition, ["`uri-id` < ?", $request['max_id']]);
-		}
-
-		if (!empty($request['since_id'])) {
-			$condition = DBA::mergeConditions($condition, ["`uri-id` > ?", $request['since_id']]);
-		}
-
-		if (!empty($request['min_id'])) {
-			$condition = DBA::mergeConditions($condition, ["`uri-id` > ?", $request['min_id']]);
-			$params['order'] = ['uri-id'];
-		}
-
 		if ($request['exclude_replies']) {
 			$condition = DBA::mergeConditions($condition, ['gravity' => Item::GRAVITY_PARENT]);
 		}
@@ -96,13 +86,15 @@ class PublicTimeline extends BaseApi
 
 		$statuses = [];
 		while ($item = Post::fetch($items)) {
-			self::setBoundaries($item['uri-id']);
 			try {
-				$statuses[] = DI::mstdnStatus()->createFromUriId($item['uri-id'], $uid, $display_quotes);
-			} catch (\Exception $exception) {
-				Logger::info('Post not fetchable', ['uri-id' => $item['uri-id'], 'uid' => $uid, 'exception' => $exception]);
+				$status =  DI::mstdnStatus()->createFromUriId($item['uri-id'], $uid, $display_quotes);
+				$this->updateBoundaries($status, $item, $request['friendica_order']);
+				$statuses[] = $status;
+			} catch (\Throwable $th) {
+				Logger::info('Post not fetchable', ['uri-id' => $item['uri-id'], 'uid' => $uid, 'error' => $th]);
 			}
 		}
+
 		DBA::close($items);
 
 		if (!empty($request['min_id'])) {
