33f2e933e2c660947fd273e0b29abf80cc71f96a
Josh Soref <2119212+jsoref@users.noreply.github.com>
2023-03-22 00:08:01 -0400

    spelling: possibility
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- src/Content/Text/BBCode.php
+++ src/Content/Text/BBCode.php
@@ -1373,7 +1373,7 @@ class BBCode
 					});
 				}
 
-				// leave open the posibility of [map=something]
+				// leave open the possibility of [map=something]
 				// this is replaced in Item::prepareBody() which has knowledge of the item location
 				if (strpos($text, '[/map]') !== false) {
 					$text = preg_replace_callback(
