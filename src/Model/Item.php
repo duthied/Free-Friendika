6c2711744450bf308c6f921b9fcf0c21c3a15390
Josh Soref <2119212+jsoref@users.noreply.github.com>
2023-03-21 23:17:28 -0400

    spelling: diaspora
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- src/Model/Item.php
+++ src/Model/Item.php
@@ -875,7 +875,7 @@ class Item
 		/*
 		 * Do we already have this item?
 		 * We have to check several networks since Friendica posts could be repeated
-		 * via OStatus (maybe Diasporsa as well)
+		 * via OStatus (maybe Diaspora as well)
 		 */
 		$duplicate = self::getDuplicateID($item);
 		if ($duplicate) {
