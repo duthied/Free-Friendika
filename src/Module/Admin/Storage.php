73aaedac977b51486d5b6a73a34ca9014f46b4f3
Philipp <admin+Github@philipp.info>
2023-03-27 19:30:28 +0200

    Apply suggestions from code review
    Co-authored-by: Hypolite Petovan <hypolite@mrpetovan.com>

--- src/Module/Admin/Storage.php
+++ src/Module/Admin/Storage.php
@@ -129,7 +129,6 @@ class Storage extends BaseAdmin
 				'prefix' => $storage_form_prefix,
 				'form'   => $storage_form,
 				'active' => $current_storage_backend instanceof ICanWriteToStorage && $name === $current_storage_backend::getName(),
-				'is_writable' => DI::config()->isWritable('storage', 'name'),
 			];
 		}
 
@@ -147,6 +146,7 @@ class Storage extends BaseAdmin
 			'$noconfig'              => DI::l10n()->t('This backend doesn\'t have custom settings'),
 			'$form_security_token'   => self::getFormSecurityToken("admin_storage"),
 			'$storagebackend_ro_txt' => !DI::config()->isWritable('storage', 'name') ? DI::l10n()->t('Changing the current backend is prohibited because it is set by an environment variable') : '',
+			'$is_writable' => DI::config()->isWritable('storage', 'name'),
 			'$storagebackend'        => $current_storage_backend instanceof ICanWriteToStorage ? $current_storage_backend::getName() : DI::l10n()->t('Database (legacy)'),
 			'$availablestorageforms' => $available_storage_forms,
 		]);
