e7e56bea655358fdcebe4978dd39a056d3957e4d
Tobias Diekershoff <tobias.diekershoff@gmx.net>
2023-04-27 07:00:31 +0200

    bump version to 2023.04-1

--- src/App.php
+++ src/App.php
@@ -64,7 +64,7 @@ class App
 {
 	const PLATFORM = 'Friendica';
 	const CODENAME = 'Giant Rhubarb';
-	const VERSION  = '2023.06-dev';
+	const VERSION  = '2023.04-1';
 
 	// Allow themes to control internal parameters
 	// by changing App values in theme.php
