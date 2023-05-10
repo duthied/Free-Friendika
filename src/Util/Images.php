3554b615084fd8704ee9b95aa538c47099d5bce5
Michael <heluecht@pirati.ca>
2023-05-10 08:48:21 +0000

    Fix tests

--- src/Util/Images.php
+++ src/Util/Images.php
@@ -343,7 +343,7 @@ class Images
 	 * @param string $preview Preview image URL
 	 * @param string $description
 	 * @return string
-	 */	public static function getBBCodeByUrl(string $photo, string $preview = null, string $description = ''): string
+	 */	public static function getBBCodeByUrl(string $photo, string $preview = null, string $description = null): string
 	{
 		if (!empty($preview)) {
 			return '[url=' . $photo . '][img=' . $preview . ']' . $description . '[/img][/url]';
