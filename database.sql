efb055f5d6697421181311908973335e28dd375b
Michael <heluecht@pirati.ca>
2023-05-19 17:36:15 +0000

    Updated messages.po and database.sql

--- database.sql
+++ database.sql
@@ -1,5 +1,5 @@
 -- ------------------------------------------
--- Friendica 2023.06-dev (Giant Rhubarb)
+-- Friendica 2023.05-rc (Giant Rhubarb)
 -- DB_UPDATE_VERSION 1518
 -- ------------------------------------------
 
