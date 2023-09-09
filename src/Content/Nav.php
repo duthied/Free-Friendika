72b68abe7f5958b1790ec301d56864a2eae19c4c
Michael <heluecht@pirati.ca>
2023-09-09 12:01:35 +0000

    Align array assignment

--- src/Content/Nav.php
+++ src/Content/Nav.php
@@ -40,21 +40,21 @@ use Friendica\Network\HTTPException;
 class Nav
 {
 	private static $selected = [
-		'global'    => null,
-		'community' => null,
-		'channel'   => null,
-		'network'   => null,
-		'home'      => null,
-		'profiles'  => null,
+		'global'        => null,
+		'community'     => null,
+		'channel'       => null,
+		'network'       => null,
+		'home'          => null,
+		'profiles'      => null,
 		'introductions' => null,
 		'notifications' => null,
-		'messages'  => null,
-		'directory' => null,
-		'settings'  => null,
-		'contacts'  => null,
-		'delegation' => null,
-		'calendar'  => null,
-		'register'  => null
+		'messages'      => null,
+		'directory'     => null,
+		'settings'      => null,
+		'contacts'      => null,
+		'delegation'    => null,
+		'calendar'      => null,
+		'register'      => null
 	];
 
 	/**
