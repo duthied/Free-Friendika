931c73d3c1eb85279efc79f141dbae58df802426
Roland Häder <roland@mxchange.org>
2024-01-16 19:58:43 +0100

    Continued: - added max_response_data_size to default configuration with hard-coded value   from code

--- static/defaults.config.php
+++ static/defaults.config.php
@@ -613,6 +613,10 @@ return [
 		// xrd_timeout (Integer)
 		// Timeout in seconds for fetching the XRD links and other requests with an expected shorter timeout
 		'xrd_timeout' => 20,
+
+		// max_response_data_size (Integer)
+		// Maximum allowed response data size in Bytes, default is hard-coded value from code
+		'max_response_data_size' => 1000000,
 	],
 	'proxy' => [
 		// forwarded_for_headers (String)
