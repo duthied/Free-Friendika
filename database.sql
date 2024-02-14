1ea8a4042d97d68ee0c8edb296d8c4a6c182485b
Tobias Diekershoff <tobias.diekershoff@gmx.net>
2024-02-14 08:24:41 +0100

    bump version to 2024.03-rc

--- database.sql
+++ database.sql
@@ -1,5 +1,5 @@
 -- ------------------------------------------
--- Friendica 2024.03-dev (Yellow Archangel)
+-- Friendica 2024.03-rc (Yellow Archangel)
 -- DB_UPDATE_VERSION 1552
 -- ------------------------------------------
 
--- src/App.php
+++ src/App.php
@@ -64,7 +64,7 @@ class App
 {
 	const PLATFORM = 'Friendica';
 	const CODENAME = 'Yellow Archangel';
-	const VERSION  = '2024.03-dev';
+	const VERSION  = '2024.03-rc';
 
 	// Allow themes to control internal parameters
 	// by changing App values in theme.php
